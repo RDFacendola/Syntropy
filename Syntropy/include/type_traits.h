ca3b7f3e9156075b9e7c572d68b897229369ea8a
Raffaele Daniele Facendola <raffaele.facendola@live.com>
2016-08-27 22:28:19 +0200

    replace<T,R> type trait has been implemented.

--- Syntropy/include/type_traits.h
+++ Syntropy/include/type_traits.h
@@ -55,44 +55,131 @@ namespace syntropy {
     template <typename TStream, typename TType>
     constexpr bool is_stream_extractable_v = is_stream_extractable<TStream, TType>::value;
         
+    //////////////// REMOVE ALL CV POINTER ////////////////
+
+    /// \brief Provides a member typedef which is the same a TType except that any pointer and qualifiers are removed recursively.
+    template <typename TType>
+    struct remove_all_cv_pointer {
+
+        using type = typename TType;
+
+    };
+
+    /// \brief Helper type form remove_all_cv_pointer<TType>.
+    template <typename TType>
+    using remove_all_cv_pointer_t = typename remove_all_cv_pointer<TType>::type;
+
+    template <typename TType>
+    struct remove_all_cv_pointer<TType*> : remove_all_cv_pointer<TType> {};
+
+    template <typename TType>
+    struct remove_all_cv_pointer<TType const> : remove_all_cv_pointer<TType> {};
+
+    template <typename TType>
+    struct remove_all_cv_pointer<TType volatile> : remove_all_cv_pointer<TType> {};
+
+    template <typename TType>
+    struct remove_all_cv_pointer<TType const volatile> : remove_all_cv_pointer<TType> {};
+    
     //////////////// DROP ////////////////
 
-    /// \brief Provides a member typedef which is the same a TType except that any pointer, qualifiers and references are removed recursively.
+    /// \brief Provides a member typedef which is the same a TType except that any pointer, extents, references and qualifiers are removed recursively.
     template <typename TType>
     struct drop {
 
-        using type = TType;
-
+        using type = typename remove_all_cv_pointer_t<std::remove_all_extents_t<std::remove_reference_t<TType>>>;
+        
     };
     
     /// \brief Helper type for drop<TType>.
     template <typename TType>
     using drop_t = typename drop<TType>::type;  
+    
+    //////////////// REPLACE ////////////////
 
-    /// \brief Specialization of drop<TType> used to strip pointers from TType.
-    template <typename TType>
-    struct drop<TType*> : drop<TType> {};
+    /// \brief Provides a member typedef which is the same a TType except that the class name is replaced with TReplace if TType is non-reference non-array type, otherwise the type is TReplace.
+    /// For example: replace_cv_pointer<int ** const *, float> has a member type 'float ** const *'.
+    /// \author Raffaele D. Facendola - August 2016
+    template <typename TType, typename TReplace>
+    struct replace_cv_pointer {
+        
+        using type = typename TReplace;
 
-    /// \brief Specialization of drop<TType> used to strip const qualifier from TType.
-    template <typename TType>
-    struct drop<TType const> : drop<TType> {};
+    };
 
-    /// \brief Specialization of drop<TType> used to strip volatile qualifier from TType.
-    template <typename TType>
-    struct drop<TType volatile> : drop<TType> {};
+    /// \brief Helper type replace_cv_pointer<TType, TReplace>.
+    template <typename TType, typename TReplace>
+    using replace_cv_pointer_t = typename replace_cv_pointer<TType, TReplace>::type;
 
-    /// \brief Specialization of drop<TType> used to strip const volatile qualifier from TType.
-    /// \remarks This specialization is needed otherwise drop<TType const> and drop<TType volatile> would be ambiguous when TType has both const and volatile qualifiers.
-    template <typename TType>
-    struct drop<TType const volatile> : drop<TType> {};
+    template <typename TType, typename TReplace>
+    struct replace_cv_pointer<TType*, TReplace> {
 
-    /// \brief Specialization of drop<TType> used to strip l-value references from TType.
-    template <typename TType>
-    struct drop<TType &> : drop<TType> {};
+        using type = std::add_pointer_t<typename replace_cv_pointer_t<TType, TReplace>>;
 
-    /// \brief Specialization of drop<TType> used to strip r-value references from TType.
-    template <typename TType>
-    struct drop<TType &&> : drop<TType> {};
+    };
+
+    template <typename TType, typename TReplace>
+    struct replace_cv_pointer<TType const, TReplace> {
+
+        using type = std::add_const_t<typename replace_cv_pointer_t<TType, TReplace>>;
+
+    };
+
+    template <typename TType, typename TReplace>
+    struct replace_cv_pointer<TType volatile, TReplace> {
+
+        using type = std::add_volatile_t<typename replace_cv_pointer_t<TType, TReplace>>;
+
+    };
+
+    template <typename TType, typename TReplace>
+    struct replace_cv_pointer<TType const volatile, TReplace> {
+
+        using type = std::add_cv_t<typename replace_cv_pointer_t<TType, TReplace>>;
+
+    };
+
+    /// \brief Provides a member typedef which is the same a TType except that the class name is replaced with TReplace.
+    /// For example: replace_cv_pointer<int ** const *[1][3], float> has a member type 'float ** const *[1][3]'.
+    /// \author Raffaele D. Facendola - August 2016
+    template <typename TType, typename TReplace>
+    struct replace {
+
+        using type = typename replace_cv_pointer_t<TType, TReplace>;
+
+    };
+
+    /// \brief Helper type replace<TType, TReplace>.
+    template <typename TType, typename TReplace>
+    using replace_t = typename replace<TType, TReplace>::type;
+    
+    template <typename TType, typename TReplace>
+    struct replace<TType&, TReplace> {
+
+        using type = std::add_lvalue_reference_t<typename replace_t<TType, TReplace>>;
+
+    };
+
+    template <typename TType, typename TReplace>
+    struct replace<TType&&, TReplace> {
+
+        using type = std::add_rvalue_reference_t<typename replace_t<TType, TReplace>>;
+
+    };
+
+    template <typename TType, typename TReplace>
+    struct replace<TType[], TReplace> {
+
+        using type = typename replace_t<TType, TReplace>[];
+
+    };
+
+    template <typename TType, typename TReplace, size_t N>
+    struct replace<TType[N], TReplace> {
+
+        using type = typename replace_t<TType, TReplace>[N];
+
+    };
 
     //////////////// IDENTITY ////////////////
     
