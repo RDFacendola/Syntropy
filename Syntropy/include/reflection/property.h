cf00c99f9008567db018341dd5a63f8495153ec5
Raffaele Daniele Facendola <raffaele.facendola@live.com>
2016-09-22 13:55:05 +0200

    Adding documentation to syntropy/reflection/property.h

--- Syntropy/include/reflection/property.h
+++ Syntropy/include/reflection/property.h
@@ -1,6 +1,6 @@
 
 /// \file property.h
-/// \brief TODO: Add brief description here
+/// \brief This header is part of the syntropy reflection system. It contains classes used to handle class properties.
 ///
 /// \author Raffaele D. Facendola - 2016
 
@@ -23,6 +23,9 @@ namespace syntropy {
 
     namespace reflection {
 
+        /// \brief Describes a class property.
+        /// A property can either be a member field, a getter method (for read-only properties) or a getter\setter method pairs.
+        /// \author Raffaele D. Facendola - 2016
         class Property {
 
         public:
@@ -32,29 +35,56 @@ namespace syntropy {
             template <typename... TAccessors>
             class PropertyT;
 
+            /// \brief Create a new named property.
             Property(const char* name);
 
             /// \brief Default destructor.
             virtual ~Property() = default;
 
+            /// \brief Get the property name.
+            /// \return Returns the property name.
             const HashedString& GetName() const noexcept;
 
             /// \brief Get the property type.
             /// \brief Returns the property type.
             virtual const Type& GetType() const noexcept = 0;
             
+            /// \brief Get the value of the property for the specified instance.
+            /// The method fails if the instance doesn't define the specified property or the specified value cannot contain the property value due to a wrong type.
+            /// \param instance Instance to read the property from.
+            /// \param value If the method succeeds contains the property value.
+            /// \return Returns true if the property could be read, returns false otherwise.
             template <typename TInstance, typename TValue>
             bool Get(const TInstance& instance, TValue&& value) const;
 
+            /// \brief Set the value of the property for the specified instance.
+            /// The method fails if the instance doesn't define the specified property or the specified value cannot be written due to a wrong type.
+            /// \param instance Instance to set the property to.
+            /// \param value Value to set.
+            /// \return Returns true if the property could be written, returns false otherwise.
             template <typename TInstance, typename TValue>
             bool Set(TInstance&& instance, const TValue& value) const;
 
+            /// \brief Add a new interface to the property.
+            /// The method creates an instance of TInstance using TArgs as construction parameters.
+            /// TInstance must be copy constructible.
+            /// Only one interface per type can be added.
+            /// \usage property.AddInterface<Foo>(bar, baz);            // Assign an instance Foo(bar, baz) to the property
+            ///        property.GetInterface<Foo>()->DoFoo();           // Query for the property for a specified interface.
+            /// \param arguments Arguments to pass to the constructor of TInterface.
+            /// \return Returns true if the method succeeds, return false if there was another interface of type TInterface.
             template <typename TInterface, typename... TArgs>
             bool AddInterface(TArgs&&... arguments);
 
+            /// \brief Query the property for an interface of type TInterface.
+            /// \return If an interface of type TInterface was previously added via AddInterface(.), returns a pointer to that interface, otherwise returns nullptr.
+            /// \remarks This method doesn't account for polymorphism. If a class of type Foo derived from Bar is added to the property, GetInterface<Bar>() will return nullptr even if a conversion exists.
             template <typename TInterface>
             const TInterface* GetInterface() const;
 
+            /// \brief Query the property for an interface of type TInterface.
+            /// \return If an interface of type TInterface was previously added via AddInterface(.), returns a pointer to that interface, otherwise returns nullptr.
+            /// \remarks This method doesn't account for polymorphism. If a class of type Foo derived from Bar is added to the property, GetInterface<Bar>() will return nullptr even if a conversion exists.
             template <typename TInterface>
             TInterface* GetInterface();
             
@@ -88,6 +118,8 @@ namespace syntropy{
 
         //////////////// PROPERTY :: PROPERTY T ////////////////
         
+        /// \brief Template specialization of PropertyT to handle member fields.
+        /// \author Raffaele D. Facendola - 2016
         template <typename TClass, typename TField>
         class Property::PropertyT<TField (TClass::*)> : public Property {
 
@@ -156,6 +188,8 @@ namespace syntropy{
 
         };
 
+        /// \brief Template specialization of PropertyT to handle read-only properties via a getter method.
+        /// \author Raffaele D. Facendola - 2016
         template <typename TClass, typename TProperty>
         class Property::PropertyT<TProperty(TClass::*)() const> : public Property {
 
@@ -213,6 +247,10 @@ namespace syntropy{
 
         };
 
+        /// \brief Template specialization of PropertyT to handle properties via a getter\setter method pair.
+        /// The getter has the form of 'TProperty Getter() const'
+        /// The setter has the form of '? Setter(TProperty)'
+        /// \author Raffaele D. Facendola - 2016
         template <typename TClass, typename TProperty, typename TReturn>
         class Property::PropertyT<TProperty(TClass::*)() const, TReturn(TClass::*)(TProperty)> : public Property {
 
@@ -284,6 +322,10 @@ namespace syntropy{
 
         };
 
+        /// \brief Template specialization of PropertyT to handle properties via a getter\setter method pair.
+        /// The getter has the form of 'const TProperty& Getter() const'
+        /// The setter has the form of 'TProperty& Setter()'
+        /// \author Raffaele D. Facendola - 2016
         template <typename TClass, typename TProperty>
         class Property::PropertyT<const TProperty&(TClass::*)() const, TProperty&(TClass::*)()> : public Property {
 
