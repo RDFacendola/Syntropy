4e774d8896b1e1b63b8b24dc9dc9e24ca30ca907
Raffaele Daniele Facendola <raffaele.facendola@live.com>
2016-10-03 15:01:47 +0200

    Renaming solution to "Syntropy". Renaming "ProtoDW" project to "UnitTest".

---
+++ UnitTest/src/unit_test.cpp
@@ -0,0 +1,29 @@
+// ProtoDW.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#include "syntropy.h"
+
+#include "unit1.h"
+#include "unit2.h"
+
+#include <iostream>
+#include <memory>
+#include <typeinfo>
+#include <typeindex>
+#include <unordered_map>
+#include <string>
+
+int main()
+{
+    
+    Tester t;
+
+    t.Do();
+    
+    system("pause");
+
+    return 0;
+
+}
