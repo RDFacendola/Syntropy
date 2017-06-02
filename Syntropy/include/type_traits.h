c3a9a7f3fdb8e3ffc392032b6d95432f0331f01b
Raffaele Daniele Facendola <raffaele.facendola@live.com>
2017-06-02 17:50:22 +0200

    Adding utility type traits to count a type's level of indirections and enumerate array extents such that runtime query is possible.

--- Syntropy/include/type_traits.h
+++ Syntropy/include/type_traits.h
@@ -16,17 +16,31 @@
 #include <map>
 #include <unordered_set>
 #include <unordered_map>
+#include <array>
 
 namespace syntropy
 {
+
+
+    /************************************************************************/
+    /* UTILITIES                                                            */
+    /************************************************************************/
+
+    /// \brief Type equal to TType without references and qualifiers.
+    template <typename TType>
+    using remove_reference_cv_t = std::remove_cv_t<std::remove_reference_t<TType>>;
+
+    /// \brief Type equal to TType without references, extents and qualifiers.
+    template <typename TType>
+    using remove_extents_reference_cv_t = std::remove_cv_t<std::remove_all_extents_t<std::remove_reference_t<TType>>>;
 
     //////////////// STREAM CAPABILITIES ////////////////
 
     /// \brief If TStream& << TType is defined provides the members constant value equal to true, otherwise value is false.
     /// \author Raffaele D. Facendola - August 2016
     template <typename TStream, typename TType>
-    class is_stream_insertable {
-
+    class is_stream_insertable
+    {
         template<typename S, typename T>
         static auto test(int) -> decltype(std::declval<S&>() << std::declval<T>(), std::true_type());
 
@@ -42,8 +56,8 @@ namespace syntropy
     /// \brief If TStream& >> TType& is defined provides the members constant value equal to true, otherwise value is false.
     /// \author Raffaele D. Facendola - August 2016
     template <typename TStream, typename TType>
-    class is_stream_extractable {
-
+    class is_stream_extractable
+    {
         template<typename S, typename T>
         static auto test(int) -> decltype(std::declval<S&>() >> std::declval<T&>(), std::true_type());
 
@@ -65,7 +79,8 @@ namespace syntropy
     constexpr bool is_stream_extractable_v = is_stream_extractable<TStream, TType>::value;
 
     template <typename TType, typename = void>
-    struct stream_insert {
+    struct stream_insert
+    {
 
         std::ostream& operator()(std::ostream& output_stream, const TType& /*source*/) {
 
@@ -327,12 +342,70 @@ namespace syntropy
     template <typename TType, typename TReplace>
     using replace_class_name_t = typename replace_class_name<TType, TReplace>::type;
     
-    /************************************************************************/
-    /* UTILITIES                                                            */
-    /************************************************************************/
-
-    /// \brief Type equal to TType without references and qualifiers.
+    /// \brief Provides a member typedef which is the resulting type after stripping either all references or extents, an indirection level or all qualifiers from TType (in this order).
     template <typename TType>
-    using remove_reference_cv_t = std::remove_cv_t<std::remove_reference_t<TType>>;
+    struct strip : std::conditional<std::is_array<TType>::value || std::is_reference<TType>::value,
+                                    std::remove_all_extents_t<std::remove_reference_t<TType>>,              // Remove references and extents from the outermost level (mutually exclusive)
+                                    std::conditional_t<std::is_pointer<TType>::value,
+                                                       std::remove_pointer_t<TType>,                        // Remove a pointer level (removes qualifiers as well)
+                                                       std::remove_cv_t<TType>>> {};                        // Remove const and volatile qualifiers from the innermost level
+
+    /// \brief Helper type strip<TType>.
+    template <typename TType>
+    using strip_t = typename strip<TType>::type;
+
+    namespace details
+    {
+        template <typename TType, size_t count = 0>
+        struct indirection_levels_helper : std::integral_constant<size_t, count> {};
+
+        /// \brief Specialization for pointers, increases the indirection levels by 1.
+        template <typename TType, size_t count>
+        struct indirection_levels_helper<TType*, count> : indirection_levels_helper<std::remove_cv_t<TType>, count + 1> {};
+
+        /// \brief Builds the sequence of extents for the array TType.
+        template <typename TType, size_t N, size_t... Extents>
+        struct array_extents_helper : array_extents_helper<TType, N - 1, std::extent_v<TType, N - 1>, Extents...> {};
+
+        template <typename TType, size_t... Extents>
+        struct array_extents_helper<TType, 0, Extents...>
+        {
+            static constexpr std::array<size_t, sizeof...(Extents)> value = { Extents... };
+        };
+    }
+
+    /// \brief Provides a member value representing the levels of indirection described by TType.
+    /// Arrays and references are not considered indirection levels.
+    /// For example: indirection_levels<int ** const *[1][3]> has a member value equal to 3.
+    template <typename TType>
+    struct indirection_levels : details::indirection_levels_helper<remove_extents_reference_cv_t<TType>> {};
+
+    /// \brief Helper constant for indirectin_levels.
+    template <typename TType>
+    constexpr size_t indirection_levels_v = indirection_levels<TType>::value;
+
+    /// \brief Provides a member value whose bits represent the result of TPredicate for each level of indirection of TType, starting from the innermost level.
+    /// For example: predicate_mask<std::is_const, int const ** const> has a member value equal to 101 (7)
+    ///              equivalent to:  std::is_const<int const>::value |                                      1 * 2^0
+    ///                             (std::is_const<int const *>::value << 1) |                              0 * 2^1
+    ///                             (std::is_const<int const ** const>::value << 2) |                       1 * 2^2
+    template <template <typename> typename TPredicate, typename TType, size_t mask = 0, typename = void>
+    struct predicate_mask : predicate_mask<TPredicate, strip_t<TType>, (mask << 1u) | TPredicate<TType>::value>{};
+
+    /// \brief Specialization for the last level of indirection.
+    template <template <typename> typename TPredicate, typename TType, size_t mask>
+    struct predicate_mask<TPredicate, TType, mask, std::enable_if_t<indirection_levels_v<TType> == 0 > > : std::integral_constant<size_t, (mask << 1u) | TPredicate<TType>::value> {};
+
+    /// \brief Helper constant for predicate_mask.
+    template <template <typename> typename TPredicate, typename TType>
+    constexpr size_t predicate_mask_v = predicate_mask<TPredicate, TType>::value;
+
+    /// \brief Provides a member array value with the same rank as TType, where the Nth element contains the extents of TType in the Nth dimension.
+    template <typename TType, typename = void>
+    struct array_extents : details::array_extents_helper<TType, std::rank_v<TType> > {};
+
+    /// \brief Helper constant for array_extents.
+    template <template <typename> typename TPredicate, typename TType>
+    constexpr size_t array_extents_v = array_extents<TType>::value;
 
 }
