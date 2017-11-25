90b074c9e3773e4fa3e62347e8e9f918a1062f7d
Raffaele Daniele Facendola <raffaele.facendola@live.com>
2017-11-25 18:21:46 +0100

    Clearning up AStar function.

--- Synapse/include/algorithms/search/astar.h
+++ Synapse/include/algorithms/search/astar.h
@@ -24,42 +24,35 @@ namespace syntropy::synapse
 	/// \param heuristic_func The estimate cost ( h(x) ) from node A to node B. THeuristicFunc(const TNode& A, const TNode& B) -> TCostType
 	/// \return the vector containing the path, in reverse order (from end to start).
 	template<typename TNode, typename TAdjacencyFunc, typename TCostFunc, typename THeuristicFunc>
-	std::vector<const TNode*> AStar(
-		const TNode& start,
-		const TNode& end,
-		TAdjacencyFunc adjacency_func,
-		TCostFunc cost_func,
-		THeuristicFunc heuristic_func)
-	{
+    std::vector<const TNode*> AStar(const TNode& start, const TNode& end, TAdjacencyFunc adjacency_func, TCostFunc cost_func, THeuristicFunc heuristic_func)
+    {
         using TCostType = decltype(heuristic_func(start, end));
 
-        std::vector<std::tuple<const TNode*, TCostType>> frontier;                                          // Nodes to be explored yet.
-		std::unordered_map<const TNode*, const TNode*> came_from;                                           // Links each node with the predecessor having the lowest cost.
-		std::unordered_map<const TNode*, TCostType> cost_to_node;                                           // Cost to reach each node from start.
+        using TFrontierNode = std::tuple<const TNode*, TCostType>;
 
-        frontier.emplace_back(&start, TCostType(0));
-		came_from.emplace(&start, &start);
-		cost_to_node.emplace(&start, TCostType(0));
-
-		while (!frontier.empty())
-		{
-			// Extract the node with the lowest cost from the frontier.
-
-            auto lowest_it = std::begin(frontier);
-
-            for (auto frontier_it = std::next(std::begin(frontier)); frontier_it != std::end(frontier); ++frontier_it)
+        struct FrontierNodeComparator
+        {
+            bool operator()(const TFrontierNode& a, const TFrontierNode& b)
             {
-                if (std::get<1>(*frontier_it) < std::get<1>(*lowest_it))
-                {
-                    lowest_it = frontier_it;
-                }
+                return std::get<1>(a) > std::get<1>(b);
             }
+        };
 
-            auto current_node = std::get<0>(*lowest_it);
+        std::priority_queue<TFrontierNode, std::vector<TFrontierNode>, FrontierNodeComparator> frontier;    // Nodes to be explored yet.
+        std::unordered_map<const TNode*, const TNode*> came_from;                                           // Links each node with the predecessor having the lowest cost.
+        std::unordered_map<const TNode*, TCostType> cost_to_node;                                           // Cost to reach each node from start.
 
-            std::iter_swap(lowest_it, std::prev(std::end(frontier)));
+        frontier.emplace(&start, TCostType(0));
+        came_from.emplace(&start, &start);
+        cost_to_node.emplace(&start, TCostType(0));
 
-            frontier.pop_back();
+        while (!frontier.empty())
+        {
+            // Extract the node with the lowest cost from the frontier.
+
+            auto current_node = std::get<0>(frontier.top());
+
+            frontier.pop();
 
             // Check if the end node was found.
 
@@ -71,7 +64,7 @@ namespace syntropy::synapse
             // Add neighbours to the frontier.
 
             auto&& neighbours = adjacency_func(*current_node);                                              // Get the neighbours of current node.
-            
+
             auto cost_to_current_node = cost_to_node.at(current_node);                                      // Cost to reach the current node from start.
 
             for (auto&& neighbour : neighbours)
@@ -79,36 +72,32 @@ namespace syntropy::synapse
                 auto new_cost = cost_to_current_node + cost_func(*current_node, *neighbour);                // Cost to node = Cost from start + cost from current to next = g(x)
 
                 if ((cost_to_node.find(neighbour) == std::end(cost_to_node)) ||                             // The node hasn't been explored yet.
-                     new_cost < cost_to_node.at(neighbour))                                                 // A better path to the node was found.
+                    new_cost < cost_to_node.at(neighbour))                                                 // A better path to the node was found.
                 {
                     cost_to_node.emplace(neighbour, new_cost);
-                    
+
                     auto priority = new_cost + heuristic_func(*neighbour, end);                             //  f(x) = g(x) + h(x)
 
-                    frontier.emplace_back(neighbour, priority);
+                    frontier.emplace(neighbour, priority);
 
                     came_from.emplace(neighbour, current_node);
                 }
-			}
-		}
+            }
+        }
 
-		// Recreate the path.
+        // Recreate the path.
 
-		std::vector<const TNode*> path;
+        std::vector<const TNode*> path;
 
-		if (came_from.find(&end) != std::end(came_from))
-		{
-            path.emplace_back(&end);
+        if (came_from.find(&end) != std::end(came_from))
+        {
+            for (auto previous_node = &end; previous_node != &start; previous_node = came_from.at(previous_node))
+            {
+                path.emplace_back(previous_node);
+            }
+        }
 
-			auto previous_node = came_from.at(&end);
+        return path;
+    }
 
-			while (previous_node != &start)
-			{
-				path.emplace_back(previous_node);
-				previous_node = came_from.at(previous_node);
-			}
-		}
-
-		return path;
-	}		
 }
