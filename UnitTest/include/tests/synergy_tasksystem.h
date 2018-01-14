2d6664de5056409639e5dc82b498ecfc88916339
Raffaele Daniele Facendola <raffaele.facendola@live.com>
2018-01-14 18:10:29 +0100

    Adding synergy.tasksystem test suite. Moving multithread testing code inside synergy.tasksystem test suite. Cleaning up main.cpp.

---
+++ UnitTest/include/tests/synergy_tasksystem.h
@@ -0,0 +1,30 @@
+
+/// \file synatropy_memory_allocators.h
+///
+/// \author Raffaele D. Facendola - 2018
+
+#pragma once
+
+#include "unit_test/test_fixture.h"
+#include "unit_test/test_case.h"
+
+#include <vector>
+
+/************************************************************************/
+/* TEST SYNERGY TASK SYSTEM                                             */
+/************************************************************************/
+
+/// \brief Test suite used to test Synergy task system.
+class TestSynergyTaskSystem : public syntropy::TestFixture
+{
+public:
+
+    static std::vector<syntropy::TestCase> GetTestCases();
+
+    /// \brief Test Synergy task graph.
+    void TestTaskGraph();
+
+private:
+
+
+};
--- UnitTest/src/main.cpp
+++ UnitTest/src/main.cpp
@@ -13,30 +13,13 @@
 #include "containers/hashed_string.h"
 #include "containers/context.h"
 
-#include "vm/virtual_machine.h"
-#include "vm/intrinsics.h"
-
-
-
 #include "diagnostics/log.h"
 #include "diagnostics/log_channels.h"
 #include "diagnostics/diagnostics_meta.h"
 
 #include "serialization/json/json.h"
 
-#include "platform/system.h"
-#include "platform/threading.h"
-
-#include "patterns/observable.h"
-
-#include "time/calendar.h"
-#include "time/timer.h"
-
 #include "math/random.h"
-
-#include "synergy.h"
-#include "task/scheduler.h"
-#include "patterns/sync_counter.h"
 
 #include "application/command_line.h"
 
@@ -61,92 +44,6 @@ void ReflectionAndSerializationTest()
     t.Do();
 }
 
-#define COUNT 1 << 16
-
-struct Maxer
-{
-    Maxer(std::vector<int>& numbers, size_t begin, size_t end, int* max)
-        : numbers_(&numbers)
-        , begin_(begin)
-        , end_(end)
-        , max_(max)
-    {
-
-    }
-
-    void operator()()
-    {
-        if (end_ - begin_ < 2)
-        {
-            *max_ = (*numbers_)[begin_];
-            return;
-        }
-
-        size_t mid = (end_ + begin_) >> 1;
-
-        auto max_l = new int;
-        auto max_r = new int;
-
-        auto l = syntropy::synergy::EmplaceTask<Maxer>({}, *numbers_, begin_, mid, max_l);
-        auto r = syntropy::synergy::EmplaceTask<Maxer>({}, *numbers_, mid, end_, max_r);
-
-        syntropy::synergy::CreateTaskContinuation({l, r},
-            [max_l, max_r, max_lr = max_, be = begin_, en = end_]()
-        {
-            *max_lr = std::max(*max_l, *max_r);
-
-            delete max_l;
-            delete max_r;
-
-            if (be == 0 && en == COUNT)
-            {
-                std::cout << "Maximum value found: " << *max_lr;
-            }
-        });
-    }
-
-    std::vector<int>* numbers_;
-    size_t begin_ = 0;
-    size_t end_ = 0;
-    int* max_ = 0;
-};
-
-void MultithreadTest()
-{
-    using namespace std::literals::chrono_literals;
-
-    const size_t count = COUNT;
-
-    std::vector<int> numbers(count);
-
-    srand(0);       // Be sure the random sequence stays the same for different runs.
-
-    for (auto&& number : numbers)
-    {
-        number = rand() % 65536;
-    }
-
-    int max;
-
-    auto& scheduler = syntropy::synergy::GetScheduler();
-
-    syntropy::synergy::GetScheduler().Initialize();
-
-    syntropy::synergy::DetachTask(
-    [&numbers, &max]()
-    {
-        std::cout << "Starting: " << std::this_thread::get_id() << "\n";
-
-        syntropy::synergy::EmplaceTask<Maxer>({}, numbers, 0, numbers.size(), &max);
-    });
-
-    system("pause");
-
-    SYNTROPY_UNUSED(scheduler);
-
-    system("pause");
-}
-
 int main(int argc, char **argv)
 {
     syntropy::CommandLine command_line(argc, argv);
@@ -156,11 +53,6 @@ int main(int argc, char **argv)
     if (command_line.HasArgument("test_reflection"))
     {
         ReflectionAndSerializationTest();
-    }
-
-    if (command_line.HasArgument("test_synergy"))
-    {
-        MultithreadTest();
     }
 
     std::cout << "\n\n";
