f5d20256df1dd4a54eceb076290063a05a358ad1
Raffaele Daniele Facendola <raffaele.facendola@live.com>
2018-01-15 00:23:28 +0100

    Cleaning up reflection\property.h.

--- Syntropy/include/reflection/property.h
+++ Syntropy/include/reflection/property.h
@@ -38,27 +38,60 @@ namespace syntropy
             /// \param name Name of the property.
             /// \param field Pointer to a member field.
             template <typename TClass, typename TField>
-            Property(const HashedString& name, TField(TClass::* field));
+            Property(const HashedString& name, TField(TClass::* field))
+                : name_(name)
+                , type_(TypeOf<TField>())
+            {
+
+                // Add interface for read\write.
+                ConditionalAddInterface<std::is_copy_constructible_v<std::remove_cv_t<TField>>, Readable>(interfaces_, field);
+                ConditionalAddInterface<std::is_copy_assignable_v<TField>, Writeable>(interfaces_, field);
+            }
 
             /// \brief Create a new property from a read-only getter.
             /// \param name Name of the property.
             /// \param getter Pointer to the member function used as property getter.
             template <typename TClass, typename TProperty>
-            Property(const HashedString& name, TProperty(TClass::* getter)() const);
+            Property(const HashedString& name, TProperty(TClass::* getter)() const)
+                : name_(name)
+                , type_(TypeOf<remove_reference_cv_t<TProperty>>())
+            {
+
+                // Add interface for read.
+                ConditionalAddInterface<std::is_copy_constructible_v<remove_reference_cv_t<TProperty>>, Readable>(interfaces_, getter);
+            }
 
             /// \brief Create a new property from a getter\setter pair.
             /// \param name Name of the property.
             /// \param getter Pointer to the member function used as property getter.
             /// \param setter Pointer to the member function used as property setter.
             template <typename TClass, typename TPropertyGetter, typename TPropertySetter>
-            Property(const HashedString& name, TPropertyGetter(TClass::* getter)() const, void (TClass::* setter)(TPropertySetter));
+            Property(const HashedString& name, TPropertyGetter(TClass::* getter)() const, void (TClass::* setter)(TPropertySetter))
+                : name_(name)
+                , type_(TypeOf<remove_reference_cv_t<TPropertyGetter>>())
+            {
+                static_assert(std::is_same_v<remove_reference_cv_t<TPropertyGetter>, remove_reference_cv_t<TPropertySetter>>,
+                    "TPropertyGetter and TPropertySetter must refer to the same underlying type (regardless of reference and qualifiers)");
+
+                // Add interface for read\write.
+                ConditionalAddInterface<std::is_copy_constructible_v<remove_reference_cv_t<TPropertyGetter>>, Readable>(interfaces_, getter);
+                ConditionalAddInterface<std::is_copy_constructible_v<remove_reference_cv_t<TPropertySetter>>, Writeable>(interfaces_, setter);
+            }
 
             /// \brief Create a new property from a getter\setter pair.
             /// \param name Name of the property.
             /// \param getter Pointer to the member function used as const accessor to the property.
             /// \param setter Pointer to the member function used as non-const accessor to the property.
             template <typename TClass, typename TProperty>
-            Property(const HashedString& name, const TProperty&(TClass::* getter)() const, TProperty& (TClass::* setter)());
+            Property(const HashedString& name, const TProperty&(TClass::* getter)() const, TProperty& (TClass::* setter)())
+                : name_(name)
+                , type_(TypeOf<TProperty>())
+            {
+
+                // Add interface for read\write.
+                ConditionalAddInterface<std::is_copy_constructible_v<TProperty>, Readable>(interfaces_, getter);
+                ConditionalAddInterface<std::is_copy_assignable_v<TProperty>, Writeable>(interfaces_, setter);
+            }
 
             /// \brief Move constructor.
             Property(Property&& other);
@@ -78,7 +111,10 @@ namespace syntropy
             /// \return If an interface of type TInterface was previously added via AddInterface(.), returns a pointer to that interface, otherwise returns nullptr.
             /// \remarks This method doesn't account for polymorphism. If a class of type Foo derived from Bar is added to the property, GetInterface<Bar>() will return nullptr even if a conversion exists.
             template <typename TInterface>
-            const TInterface* GetInterface() const;
+            const TInterface* GetInterface() const
+            {
+                return interfaces_.GetInterface<TInterface>();
+            }
 
         private:
 
@@ -100,128 +136,44 @@ namespace syntropy
 
             /// \brief Create a new property definition.
             /// \param accessor Accessors to the property.
-            PropertyDefinitionT(Property& subject, TAccessors... accessors);
+            PropertyDefinitionT(Property& subject, TAccessors... accessors)
+                : property_(subject)
+                , arguments_(*this, std::forward<TAccessors>(accessors)...)
+            {
+
+            }
 
             /// \brief Add a new interface to the property.
             /// The method creates an instance of TConcrete using TArgs as construction parameters. Only one interface of type TInterface can be added per property.
             /// TConcrete must be equal to or derive from TInterface.
             /// \param arguments Arguments to pass to the constructor of TInterface.
             template <typename TInterface, typename TConcrete = TInterface, typename... TArgs>
-            PropertyDefinitionT& AddInterface(TArgs&&... arguments);
+            PropertyDefinitionT& AddInterface(TArgs&&... arguments)
+            {
+                if (property_.interfaces_.AddInterface<TInterface, TConcrete>(std::forward<TArgs>(arguments)...) == nullptr)
+                {
+                    SYNTROPY_ERROR((ReflectionCtx), "An interface '", typeid(TInterface).name(), "' was already added to the property '", property_.name_, "'. The new interface has been ignored.");
+                }
+
+                return *this;
+            }
 
             /// \brief Apply a functor to this property definition.
             template <typename TFunctor>
-            PropertyDefinitionT& operator<<(TFunctor&& functor);
+            PropertyDefinitionT& operator<<(TFunctor&& functor)
+            {
+                std::apply(std::forward<TFunctor>(functor), arguments_);
+                return *this;
+            }
 
         private:
 
-            Property& subject_;                                         ///< \brief Property this definition refers to.
+            Property& property_;                                        ///< \brief Property this definition refers to.
 
             using TArguments = std::tuple<PropertyDefinitionT<TAccessors...>&, TAccessors...>;
 
             TArguments arguments_;                                      ///< \brief Arguments passed to the functors (a reference to this object and any property accessor).
-
         };
 
     }
-}
-
-namespace syntropy
-{ 
-    namespace reflection
-    {
-
-        /************************************************************************/
-        /* PROPERTY                                                             */
-        /************************************************************************/
-
-        template <typename TClass, typename TField>
-        Property::Property(const HashedString& name, TField(TClass::* field))
-            : name_(name)
-            , type_(TypeOf<TField>())
-        {
-
-            // Add interface for read\write.
-            ConditionalAddInterface<std::is_copy_constructible_v<std::remove_cv_t<TField>>, Readable>(interfaces_, field);
-            ConditionalAddInterface<std::is_copy_assignable_v<TField>, Writeable>(interfaces_, field);
-
-        }
-
-        template <typename TClass, typename TProperty>
-        Property::Property(const HashedString& name, TProperty(TClass::* getter)() const)
-            : name_(name)
-            , type_(TypeOf<remove_reference_cv_t<TProperty>>())
-        {
-
-            // Add interface for read.
-            ConditionalAddInterface<std::is_copy_constructible_v<remove_reference_cv_t<TProperty>>, Readable>(interfaces_, getter);
-
-        }
-
-        template <typename TClass, typename TPropertyGetter, typename TPropertySetter>
-        Property::Property(const HashedString& name, TPropertyGetter(TClass::* getter)() const, void (TClass::* setter)(TPropertySetter))
-            : name_(name)
-            , type_(TypeOf<remove_reference_cv_t<TPropertyGetter>>())
-        {
-            static_assert(std::is_same_v<remove_reference_cv_t<TPropertyGetter>, remove_reference_cv_t<TPropertySetter>>, 
-                          "TPropertyGetter and TPropertySetter must refer to the same underlying type (regardless of reference and qualifiers)");
-
-            // Add interface for read\write.
-            ConditionalAddInterface<std::is_copy_constructible_v<remove_reference_cv_t<TPropertyGetter>>, Readable>(interfaces_, getter);
-            ConditionalAddInterface<std::is_copy_constructible_v<remove_reference_cv_t<TPropertySetter>>, Writeable>(interfaces_, setter);
-
-        }
-
-        template <typename TClass, typename TProperty>
-        Property::Property(const HashedString& name, const TProperty&(TClass::* getter)() const, TProperty& (TClass::* setter)())
-            : name_(name)
-            , type_(TypeOf<TProperty>())
-        {
-
-            // Add interface for read\write.
-            ConditionalAddInterface<std::is_copy_constructible_v<TProperty>, Readable>(interfaces_, getter);
-            ConditionalAddInterface<std::is_copy_assignable_v<TProperty>, Writeable>(interfaces_, setter);
-
-        }
-
-        template <typename TInterface>
-        const TInterface* Property::GetInterface() const
-        {
-            return interfaces_.GetInterface<TInterface>();
-        }
-
-        /************************************************************************/
-        /* PROPERTY DEFINITION T <TACCESSORS...>                                */
-        /************************************************************************/
-
-        template <typename... TAccessors>
-        PropertyDefinitionT<TAccessors...>::PropertyDefinitionT(Property& subject, TAccessors... accessors)
-            : subject_(subject)
-            , arguments_(*this, std::forward<TAccessors>(accessors)...)
-        {
-
-        }
-
-        template <typename... TAccessors>
-        template <typename TInterface, typename TConcrete, typename... TArgs>
-        PropertyDefinitionT<TAccessors...>& PropertyDefinitionT<TAccessors...>::AddInterface(TArgs&&... arguments)
-        {
-            if (subject_.interfaces_.AddInterface<TInterface, TConcrete>(std::forward<TArgs>(arguments)...) == nullptr)
-            {
-                SYNTROPY_ERROR((ReflectionCtx), "An interface '", typeid(TInterface).name(), "' was already added to the property '", subject_.name_, "'. The new interface has been ignored.");
-            }
-
-            return *this;
-        }
-
-        template <typename... TAccessors>
-        template <typename TFunctor>
-        PropertyDefinitionT<TAccessors...>& PropertyDefinitionT<TAccessors...>::operator<<(TFunctor&& functor)
-        {
-            std::apply(std::forward<TFunctor>(functor), arguments_);
-            return *this;
-        }
-
-    }
-
 }
