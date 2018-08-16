0f8a774917dd9d1a6c05b9f978c5975c7cdaa514
Raffaele Daniele Facendola <raffaele.facendola@live.com>
2018-08-16 10:35:34 +0200

    Adding syntropy::NullAllocator.

---
+++ Syntropy/include/memory/allocators/null_allocator.h
@@ -0,0 +1,102 @@
+
+/// \file null_allocator.h
+/// \brief This header is part of the syntropy memory management system. It contains empty allocators.
+///
+/// \author Raffaele D. Facendola - 2018
+
+#pragma once
+
+#include "memory/bytes.h"
+#include "memory/alignment.h"
+#include "memory/memory_address.h"
+#include "memory/memory_range.h"
+
+#include "diagnostics/assert.h"
+
+namespace syntropy
+{
+    /************************************************************************/
+    /* NULL ALLOCATOR                                                       */
+    /************************************************************************/
+
+    /// \brief Basic allocator that rejects any form of allocation.
+    /// \author Raffaele D. Facendola - August 0218
+    class NullAllocator
+    {
+    public:
+
+        /// \brief Default constructor.
+        NullAllocator() noexcept = default;
+
+        /// \brief Default copy constructor.
+        NullAllocator(const NullAllocator&) noexcept = default;
+
+        /// \brief Default move constructor.
+        NullAllocator(NullAllocator&&) noexcept = default;
+
+        /// \brief Default destructor.
+        ~NullAllocator() noexcept = default;
+
+        /// \brief Default assignment operator.
+        NullAllocator& operator=(const NullAllocator&) noexcept = default;
+
+        /// \brief Allocate a new memory block.
+        /// \param size Size of the memory block to allocate.
+        /// \return Returns an empty range.
+        MemoryRange Allocate(Bytes size) noexcept;
+
+        /// \brief Allocate a new aligned memory block.
+        /// \param size Size of the memory block to allocate.
+        /// \param alignment Block alignment.
+        /// \return Returns an empty range.
+        MemoryRange Allocate(Bytes size, Alignment alignment) noexcept;
+
+        /// \brief Deallocate a memory block.
+        /// \param block Block to deallocate. Expects an empty range.
+        void Deallocate(const MemoryRange& block);
+
+        /// \brief Deallocate an aligned memory block.
+        /// \param block Block to deallocate. Expects an empty range.
+        /// \param alignment Block alignment.
+        void Deallocate(const MemoryRange& block, Alignment alignment);
+
+        /// \brief Check whether this allocator owns the provided memory block.
+        /// The null allocator only contains empty ranges.
+        /// \return Returns true if the provided memory range is empty, returns false otherwise.
+        bool Owns(const MemoryRange& block) const noexcept;
+
+    };
+
+}
+
+/************************************************************************/
+/* IMPLEMENTATION                                                       */
+/************************************************************************/
+ 
+namespace syntropy
+{
+    MemoryRange NullAllocator::Allocate(Bytes /*size*/) noexcept
+    {
+        return {};
+    }
+
+    MemoryRange NullAllocator::Allocate(Bytes /*size*/, Alignment /*alignment*/) noexcept
+    {
+        return {};
+    }
+
+    void NullAllocator::Deallocate(const MemoryRange& block)
+    {
+        SYNTROPY_ASSERT(!block);        // Only empty ranges can be "deallocated" by this allocator.
+    }
+
+    void NullAllocator::Deallocate(const MemoryRange& block, Alignment /*alignment*/)
+    {
+        SYNTROPY_ASSERT(!block);        // Only empty ranges can be "deallocated" by this allocator.
+    }
+
+    bool NullAllocator::Owns(const MemoryRange& block) const noexcept
+    {
+        return !block;                  // This allocator "owns" only empty ranges.
+    }
+}
