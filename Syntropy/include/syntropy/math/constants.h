572eac64a5734f0686649d86f86c6e9d11a4136d
Raffaele Daniele Facendola <raffaele.facendola@live.com>
2019-08-19 11:30:48 +0200

    Adding constants.h, a file containing mathematical constants.

---
+++ Syntropy/include/syntropy/math/constants.h
@@ -0,0 +1,23 @@
+
+/// \file constants.h
+/// \brief This header is part of the syntropy math system. It contains math constants.
+///
+/// \author Raffaele D. Facendola - 2019
+
+#pragma once
+
+namespace syntropy
+{
+    /************************************************************************/
+    /* CONSTANTS                                                            */
+    /************************************************************************/
+
+    /// \brief Archimedes' constant.
+    inline const float kPi = 3.14159f;
+
+    /// \brief Two times the Archimedes' constant.
+    inline const float kTau = 2.0f * kPi;
+
+    /// \brief Euler's number.
+    inline const float kE = 2.71828f;
+}
