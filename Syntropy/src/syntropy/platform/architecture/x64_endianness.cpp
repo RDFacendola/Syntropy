a3cf9a32eaa9f1a2ff043d18b2115349b84c91a3
Raffaele Daniele Facendola <raffaele.facendola@live.com>
2019-11-10 11:31:52 +0100

    Fixing endianness conversion to avoid bad type punning.

--- Syntropy/src/syntropy/platform/architecture/x64_endianness.cpp
+++ Syntropy/src/syntropy/platform/architecture/x64_endianness.cpp
@@ -10,45 +10,36 @@ namespace syntropy
     /* X64 ENDIANNESS                                                       */
     /************************************************************************/
 
-    /// \brief Utility used to convert endianness.
-    union EndianBytes
-    {
-        /// \brief Value to encode\decode.
-        std::uint64_t value_;
-
-        /// \brief Value bytes.
-        std::uint8_t bytes_[8];
-    };
-
     std::uint16_t x64Endianness::ToBigEndian(std::uint16_t rhs)
     {
-        auto bytes = EndianBytes{rhs};
+        auto b0 = (rhs & 0xFF00) >> 8u;
+        auto b1 = (rhs & 0x00FF) >> 0u;
 
-        std::swap(bytes.bytes_[0], bytes.bytes_[1]);
-
-        return std::uint16_t(bytes.value_);
+        return std::uint16_t((b1 << 8u) | (b0 << 0u));
     }
 
     std::uint32_t x64Endianness::ToBigEndian(std::uint32_t rhs)
     {
-        auto bytes = EndianBytes{ rhs };
+        auto b0 = (rhs & 0xFF000000) >> 24u;
+        auto b1 = (rhs & 0x00FF0000) >> 16u;
+        auto b2 = (rhs & 0x0000FF00) >> 8u;
+        auto b3 = (rhs & 0x000000FF) >> 0u;
 
-        std::swap(bytes.bytes_[0], bytes.bytes_[3]);
-        std::swap(bytes.bytes_[1], bytes.bytes_[2]);
-
-        return std::uint32_t(bytes.value_);
+        return std::uint32_t((b3 << 24u) | (b2 << 16u) | (b1 << 8u) | (b0 << 0u));
     }
 
     std::uint64_t x64Endianness::ToBigEndian(std::uint64_t rhs)
     {
-        auto bytes = EndianBytes{ rhs };
+        auto b0 = (rhs & 0xFF00000000000000) >> 56u;
+        auto b1 = (rhs & 0x00FF000000000000) >> 48u;
+        auto b2 = (rhs & 0x0000FF0000000000) >> 40;
+        auto b3 = (rhs & 0x000000FF00000000) >> 32u;
+        auto b4 = (rhs & 0x00000000FF000000) >> 24u;
+        auto b5 = (rhs & 0x0000000000FF0000) >> 16u;
+        auto b6 = (rhs & 0x000000000000FF00) >> 8u;
+        auto b7 = (rhs & 0x00000000000000FF) >> 0u;
 
-        std::swap(bytes.bytes_[0], bytes.bytes_[7]);
-        std::swap(bytes.bytes_[1], bytes.bytes_[6]);
-        std::swap(bytes.bytes_[2], bytes.bytes_[5]);
-        std::swap(bytes.bytes_[3], bytes.bytes_[4]);
-
-        return bytes.value_;
+        return std::uint32_t((b7 << 56u) | (b6 << 48u) | (b5 << 40u) | (b4 << 32u) | (b3 << 24u) | (b2 << 16u) | (b1 << 8u) | (b0 << 0u));
     }
 
 }
