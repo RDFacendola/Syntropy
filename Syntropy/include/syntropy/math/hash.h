91df7d961b15de6b0145ad92c88f7e4a982179c2
Raffaele Daniele Facendola <raffaele.facendola@live.com>
2020-04-22 23:53:42 +0200

    Moving Hash implementation to header.

--- Syntropy/include/syntropy/math/hash.h
+++ Syntropy/include/syntropy/math/hash.h
@@ -7,6 +7,7 @@
 #pragma once
 
 #include <string>
+#include <numeric>
 
 namespace syntropy
 {
@@ -64,5 +65,40 @@ namespace syntropy
             return Hash::Hash32(string);
         }
     };
+
+    /************************************************************************/
+    /* IMPLEMENTATION                                                       */
+    /************************************************************************/
+
+    // Hash64.
+
+    inline uint64_t Hash::Hash64(const void* buffer, size_t length)
+    {
+        // Uses FNV1a 64-bit with recommended constants by Landon Curt Noll - http://www.isthe.com/chongo/
+
+        auto begin = reinterpret_cast<const uint8_t*>(buffer);
+
+        return std::accumulate(begin, begin + length, uint64_t(0xcbf29ce484222325U), [](uint64_t hash, uint8_t character)
+        {
+            hash ^= static_cast<uint64_t>(character);
+
+            return (hash << 0) + (hash << 1) + (hash << 4) + (hash << 5) + (hash << 7) + (hash << 8) + (hash << 40);
+        });
+    }
+
+    // Hash32.
+    inline uint32_t Hash::Hash32(const void* buffer, size_t length)
+    {
+        // Uses FNV1a 32-bit with recommended constants by Landon Curt Noll - http://www.isthe.com/chongo/
+
+        auto begin = reinterpret_cast<const uint8_t*>(buffer);
+
+        return std::accumulate(begin, begin + length, uint32_t(0x811c9dc5U), [](uint32_t hash, uint8_t character)
+        {
+            hash ^= static_cast<uint32_t>(character);
+
+            return (hash << 0) + (hash << 1) + (hash << 4) + (hash << 7) + (hash << 8) + (hash << 24);
+        });
+    }
 
 }
