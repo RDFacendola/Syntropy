a4c1867cc79768d638e9339a09ea478ef34e1ef5
Raffaele Daniele Facendola <raffaele.facendola@live.com>
2020-04-22 23:48:41 +0200

    Updating Range interface.

--- Syntropy/include/syntropy/types/range.h
+++ Syntropy/include/syntropy/types/range.h
@@ -6,6 +6,7 @@
 
 #pragma once
 
+#include <type_traits>
 #include <ostream>
 
 namespace syntropy
@@ -22,10 +23,18 @@ namespace syntropy
     {
     public:
 
-        /// \brief Create a new component range.
-        /// \param begin Begin of the range.
-        /// \param end End of the range.
+        /// \brief Value type.
+        using TValue = std::remove_reference_t<decltype(*std::declval<TIterator>())>;
+
+        /// \brief Create a new range.
+        /// \param begin Iterator to the first element in the range.
+        /// \param end Iterator past the last element in the range.
         Range(TIterator begin, TIterator end);
+
+        /// \brief Create a new range.
+        /// \param begin First element in the range.
+        /// \param size Number of elements in the range.
+        Range(TIterator begin, std::int64_t size);
 
         /// \brief Get an iterator to the first element in the range.
         TIterator Begin() const;
@@ -33,18 +42,27 @@ namespace syntropy
         /// \brief Get an iterator past the last element in the range.
         TIterator End() const;
 
-        /// \brief Reference to the element pointed by head.
-        auto operator*();
+        /// \brief Access the first element in the range.
+        TValue& GetFront() const;
 
-        /// \brief Pointer to the element pointed by head.
-        auto operator->();
+        /// \brief Access the last element in the range.
+        TValue& GetBack() const;
 
-        /// \brief Advance the range head forward by one element.
-        /// This method results in undefined behavior if the range is empty.
-        void Advance();
+        /// \brief Access an element by index.
+        TValue& operator[](std::int64_t index) const;
+
+        /// \brief Get a pointer to the beginning of the range.
+        TValue* GetData() const;
 
         /// \brief Check whether the range is empty.
         operator bool() const;
+
+        /// \brief Get the number of elements in the range.
+        std::int64_t GetSize() const;
+
+        /// \brief Advance the range head forward by one element.
+        /// This method results in undefined behavior is the range is empty.
+        void PopFront();
 
     protected:
 
@@ -68,17 +86,13 @@ namespace syntropy
     template <typename TIterator>
     TIterator end(const Range<TIterator>& range);
 
-    /// \brief Get a const iterator to the first element in the range.
-    template <typename TIterator>
-    TIterator cbegin(const Range<TIterator>& range);
-
-    /// \brief Get a const iterator past the last element in the range.
-    template <typename TIterator>
-    TIterator cend(const Range<TIterator>& range);
-
     /// \brief Create a new range from a pair of iterators.
     template <typename TIterator>
     Range<TIterator> MakeRange(TIterator begin, TIterator end);
+
+    /// \brief Create a new range from an iterator and a number of elements.
+    template <typename TIterator>
+    Range<TIterator> MakeRange(TIterator begin, std::int64_t size);
 
     /// \brief Create a new range from a collection.
     template <typename TCollection>
@@ -103,47 +117,74 @@ namespace syntropy
     // Range<TIterator>.
 
     template <typename TIterator>
-    Range<TIterator>::Range(TIterator begin, TIterator end) :
-        begin_(begin),
-        end_(end)
+    inline Range<TIterator>::Range(TIterator begin, TIterator end)
+        : begin_(begin)
+        , end_(end)
     {
     
     }
 
     template <typename TIterator>
-    TIterator Range<TIterator>::Begin() const
+    inline Range<TIterator>::Range(TIterator begin, std::int64_t count)
+        : Range(begin, begin + count)
+    {
+
+    }
+
+    template <typename TIterator>
+    inline TIterator Range<TIterator>::Begin() const
     {
         return begin_;
     }
 
     template <typename TIterator>
-    TIterator Range<TIterator>::End() const
+    inline TIterator Range<TIterator>::End() const
     {
         return end_;
     }
 
     template <typename TIterator>
-    inline auto Range<TIterator>::operator*()
+    inline typename Range<TIterator>::TValue& Range<TIterator>::GetFront() const
     {
         return *begin_;
     }
 
     template <typename TIterator>
-    typename auto Range<TIterator>::operator->()
+    inline typename Range<TIterator>::TValue& Range<TIterator>::GetBack() const
     {
-        return &(*begin_);
+        return *(end_ - 1);
     }
 
     template <typename TIterator>
-    inline void Range<TIterator>::Advance()
+    inline typename Range<TIterator>::TValue& Range<TIterator>::operator[](std::int64_t index) const
     {
-        ++begin_;
+        return GetData()[index];
+    }
+
+    template <typename TIterator>
+    inline typename Range<TIterator>::TValue* Range<TIterator>::GetData() const
+    {
+        return &GetFront();
     }
 
     template <typename TIterator>
     inline Range<TIterator>::operator bool() const
     {
-        return begin_ != end_;
+        return GetSize() > 0;
+    }
+
+    template <typename TIterator>
+    inline std::int64_t Range<TIterator>::GetSize() const
+    {
+        using std::distance;
+
+        return distance(begin_, end_);
+    }
+
+    template <typename TIterator>
+    inline void Range<TIterator>::PopFront()
+    {
+        ++begin_;
     }
 
     // Non-member functions.
@@ -161,21 +202,15 @@ namespace syntropy
     }
 
     template <typename TIterator>
-    inline TIterator cbegin(const Range<TIterator>& range)
-    {
-        return range.Begin();
-    }
-
-    template <typename TIterator>
-    inline TIterator cend(Range<TIterator>& range)
-    {
-        return range.End();
-    }
-
-    template <typename TIterator>
     inline Range<TIterator> MakeRange(TIterator begin, TIterator end)
     {
         return { begin, end };
+    }
+
+    template <typename TIterator>
+    inline Range<TIterator> MakeRange(TIterator begin, std::int64_t size)
+    {
+        return { begin, size };
     }
 
     template <typename TCollection>
