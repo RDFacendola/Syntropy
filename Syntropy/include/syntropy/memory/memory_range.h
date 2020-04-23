cbd9602f22d6e68511e8b20405090b0e35051e82
Raffaele Daniele Facendola <raffaele.facendola@live.com>
2020-04-23 20:18:03 +0200

    MemoryRange is now an alias for Range<MemoryAddress>.

--- Syntropy/include/syntropy/memory/memory_range.h
+++ Syntropy/include/syntropy/memory/memory_range.h
@@ -6,14 +6,10 @@
 
 #pragma once
 
-#include <cstdint>
-
 #include "syntropy/memory/bytes.h"
 #include "syntropy/memory/memory_address.h"
 
-#include "syntropy/diagnostics/assert.h"
-
-#include "syntropy/math/math.h"
+#include "syntropy/types/range.h"
 
 namespace syntropy
 {
@@ -21,257 +17,50 @@ namespace syntropy
     /* MEMORY RANGE                                                         */
     /************************************************************************/
 
-    /// \brief Represents a range of contiguous memory addresses.
-    /// The range is of the form [begin; end)
-    /// \author Raffaele D. Facendola - December 2016
-    template <bool is_const>
-    class MemoryRangeT
-    {
-    public:
+    /// \brief Type alias for a range of contiguous memory addresses.
+    using MemoryRange = Range<MemoryAddress>;
 
-        /// \brief Create an empty memory range.
-        constexpr MemoryRangeT() = default;
+    /// \brief Type alias for a range of constant contiguous memory addresses.
+    using ConstMemoryRange = Range<ConstMemoryAddress>;
 
-        /// \brief Create a memory range.
-        /// \param begin First address in the range.
-        /// \param end One past the last address in the range.
-        constexpr MemoryRangeT(MemoryAddressT<is_const> begin, MemoryAddressT<is_const> end);
+    /************************************************************************/
+    /* NON-MEMBER FUNCTIONS                                                 */
+    /************************************************************************/
 
-        /// \brief Create a memory range.
-        /// \param begin First address in the range.
-        /// \param size Size of the range.
-        constexpr MemoryRangeT(MemoryAddressT<is_const> begin, Bytes size);
-
-        /// \brief Default copy-constructor.
-        template <bool is_rhs_const>
-        constexpr MemoryRangeT(const MemoryRangeT<is_rhs_const>& rhs);
-
-        /// \brief Default assignment operator.
-        constexpr MemoryRangeT& operator=(const MemoryRangeT&) = default;
-
-        /// \brief Check whether the range is non-empty.
-        /// \return Returns true if the range is non-empty, returns false otherwise.
-        constexpr operator bool() const noexcept;
-
-        /// \brief Access an element in the range.
-        /// \param offset Offset with respect to the first element of the range.
-        /// \return Returns a pointer to offset-bytes after the base of the range.
-        constexpr MemoryAddressT<is_const> operator[](Bytes offset) const;
-
-        /// \brief Advance the memory range forward.
-        /// \param rhs Number of bytes to move the range forward to.
-        /// \return Returns a reference to this element.
-        constexpr MemoryRangeT& operator+=(Bytes rhs) noexcept;
-
-        /// \brief Move the memory range backwards.
-        /// \param rhs Number of bytes to move the range backwards to.
-        /// \return Returns a reference to this element.
-        constexpr MemoryRangeT& operator-=(Bytes rhs) noexcept;
-
-        /// \brief Get the base address of the range.
-        /// \return Returns the base address of the range.
-        constexpr MemoryAddressT<is_const> Begin() const noexcept;
-
-        /// \brief Get the address past the end of this range,
-        /// \return Returns the address past the end of this range.
-        constexpr MemoryAddressT<is_const> End() const noexcept;
-
-        /// \brief Get the size of the range, in bytes.
-        /// \return Returns the total capacity of the memory range, in bytes.
-        constexpr Bytes GetSize() const noexcept;
-
-        /// \brief Check whether a memory range is contained entirely inside this range.
-        /// \param memory_range Memory range to check.
-        /// \return Returns true if memory_range is contained inside this memory range, returns false otherwise.
-        constexpr bool Contains(const MemoryRangeT<false>& memory_range) const noexcept;
-
-        /// \brief Check whether an address falls within this memory range.
-        /// \param address Address to check.
-        /// \return Returns true if address is contained inside this memory range, returns false otherwise.
-        constexpr bool Contains(ConstMemoryAddress address) const noexcept;
-
-    private:
-
-        MemoryAddressT<is_const> begin_;       ///< \brief First address in the memory range.
-
-        MemoryAddressT<is_const> end_;         ///< \brief One past the last address in the memory range.
-
-    };
-
-    /// \brief Type alias for non-const memory ranges.
-    using MemoryRange = MemoryRangeT<false>;
-
-    /// \brief Type alias for const memory ranges.
-    using ConstMemoryRange = MemoryRangeT<true>;
-
-    /// \brief Equality comparison for MemoryRange.
-    /// \return Returns true if lhs and rhs refer to the same memory range, returns false otherwise.
-    template <bool is_lhs_const, bool is_rhs_const>
-    constexpr bool operator==(const MemoryRangeT<is_lhs_const>& lhs, const MemoryRangeT<is_rhs_const>& rhs) noexcept;
-
-    /// \brief Inequality comparison for MemoryRange.
-    /// \return Returns true if lhs and rhs refer to different memory ranges, returns false otherwise.
-    template <bool is_lhs_const, bool is_rhs_const>
-    constexpr bool operator!=(const MemoryRangeT<is_lhs_const>& lhs, const MemoryRangeT<is_rhs_const>& rhs) noexcept;
-
-    /// \brief Move a memory range forward.
-    /// \return Returns a memory range which is equal to lhs moved forward by rhs bytes.
-    template <bool is_const>
-    constexpr MemoryRangeT<is_const> operator+(const MemoryRangeT<is_const>& lhs, const Bytes& rhs) noexcept;
-
-    /// \brief Move a memory range backward.
-    /// \return Returns a memory range which is equal to lhs moved backwards by rhs bytes.
-    template <bool is_const>
-    constexpr MemoryRangeT<is_const> operator-(const MemoryRangeT<is_const>& lhs, const Bytes& rhs) noexcept;
-
-    /// \brief Create a memory range from data.
-    /// \param data Data to wrap.
+    /// \brief Create a memory range containing the provided object.
     template <typename TType>
-    constexpr MemoryRange MakeMemoryRange(TType& data) noexcept;
+    MemoryRange MakeMemoryRange(TType& data) noexcept;
 
-    /// \brief Create a memory range from data.
-        /// \param data Data to wrap.
+    /// \brief Create a memory range containing the provided object.
     template <typename TType>
-    constexpr ConstMemoryRange MakeMemoryRange(const TType& data) noexcept;
+    ConstMemoryRange MakeMemoryRange(const TType& data) noexcept;
 
-    /// \brief Create a memory range from data.
-    /// \param data Data to wrap.
+    /// \brief Create a constant memory range containing the provided object.
     template <typename TType>
-    constexpr ConstMemoryRange MakeConstMemoryRange(const TType& data) noexcept;
+    ConstMemoryRange MakeConstMemoryRange(const TType& data) noexcept;
 
     /************************************************************************/
     /* IMPLEMENTATION                                                       */
     /************************************************************************/
 
-    template <bool is_const>
-    constexpr MemoryRangeT<is_const>::MemoryRangeT(MemoryAddressT<is_const> begin, MemoryAddressT<is_const> end)
-        : begin_(begin)
-        , end_(end)
-    {
-        SYNTROPY_ASSERT(begin <= end);
-    }
-
-    template <bool is_const>
-    constexpr MemoryRangeT<is_const>::MemoryRangeT(MemoryAddressT<is_const> begin, Bytes size)
-        : MemoryRangeT{ begin, begin + size }
-    {
-
-    }
-
-    template <bool is_const>
-    template <bool is_rhs_const>
-    constexpr MemoryRangeT<is_const>::MemoryRangeT(const MemoryRangeT<is_rhs_const>& rhs)
-        : begin_(rhs.Begin())
-        , end_(rhs.End())
-    {
-
-    }
-
-    template <bool is_const>
-    constexpr MemoryRangeT<is_const>::operator bool() const noexcept
-    {
-        return end_ != begin_;
-    }
-
-    template <bool is_const>
-    constexpr MemoryAddressT<is_const> MemoryRangeT<is_const>::operator[](Bytes offset) const
-    {
-        auto address = begin_ + offset;
-
-        SYNTROPY_ASSERT(Contains(address));
-
-        return address;
-    }
-
-    template <bool is_const>
-    constexpr MemoryRangeT<is_const>& MemoryRangeT<is_const>::operator+=(Bytes rhs) noexcept
-    {
-        begin_ += rhs;
-        end_ += rhs;
-        return *this;
-    }
-
-    template <bool is_const>
-    constexpr MemoryRangeT<is_const>& MemoryRangeT<is_const>::operator-=(Bytes rhs) noexcept
-    {
-        SYNTROPY_ASSERT(intptr_t(begin_) >= *rhs);
-
-        begin_ -= rhs;
-        end_ -= rhs;
-        return *this;
-    }
-
-    template <bool is_const>
-    constexpr MemoryAddressT<is_const> MemoryRangeT<is_const>::Begin() const noexcept
-    {
-        return begin_;
-    }
-
-    template <bool is_const>
-    constexpr MemoryAddressT<is_const> MemoryRangeT<is_const>::End() const noexcept
-    {
-        return end_;
-    }
-
-    template <bool is_const>
-    constexpr Bytes MemoryRangeT<is_const>::GetSize() const noexcept
-    {
-        return Bytes(end_ - begin_);
-    }
-
-    template <bool is_const>
-    constexpr bool MemoryRangeT<is_const>::Contains(const MemoryRangeT<false>& memory_range) const noexcept
-    {
-        return begin_ <= memory_range.Begin() && memory_range.End() <= end_;
-    }
-
-    template <bool is_const>
-    constexpr bool MemoryRangeT<is_const>::Contains(ConstMemoryAddress address) const noexcept
-    {
-        return begin_ <= address && address < end_;
-    }
-
-    template <bool is_lhs_const, bool is_rhs_const>
-    constexpr bool operator==(const MemoryRangeT<is_lhs_const>& lhs, const MemoryRangeT<is_rhs_const>& rhs) noexcept
-    {
-        return lhs.Begin() == rhs.Begin() && lhs.End() == rhs.End();
-    }
-
-    template <bool is_lhs_const, bool is_rhs_const>
-    constexpr bool operator!=(const MemoryRangeT<is_lhs_const>& lhs, const MemoryRangeT<is_rhs_const>& rhs) noexcept
-    {
-        return !(lhs == rhs);
-    }
-
-    template <bool is_const>
-    constexpr MemoryRangeT<is_const> operator+(const MemoryRangeT<is_const>& lhs, const Bytes& rhs) noexcept
-    {
-        return MemoryRangeT(lhs) += rhs;
-    }
-
-    template <bool is_const>
-    constexpr MemoryRangeT<is_const> operator-(const MemoryRangeT<is_const>& lhs, const Bytes& rhs) noexcept
-    {
-        return MemoryRangeT(lhs) -= rhs;
-    }
+    // Non-member functions.
 
     template <typename TType>
-    constexpr MemoryRange MakeMemoryRange(TType& data) noexcept
+    MemoryRange MakeMemoryRange(TType& data) noexcept
     {
         return { &data, &data + 1 };
     }
 
     template <typename TType>
-    constexpr ConstMemoryRange MakeMemoryRange(const TType& data) noexcept
+    ConstMemoryRange MakeMemoryRange(const TType& data) noexcept
     {
         return { &data, &data + 1 };
     }
 
     template <typename TType>
-    constexpr ConstMemoryRange MakeConstMemoryRange(const TType& data) noexcept
+    ConstMemoryRange MakeConstMemoryRange(const TType& data) noexcept
     {
         return MakeMemoryRange(data);
     }
-
 
 }
