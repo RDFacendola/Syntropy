fda98de8a9af58f51bb8feb00ff843ae8cffa109
Raffaele Daniele Facendola <raffaele.facendola@live.com>
2020-04-27 08:11:28 +0200

    Removing polymorphic wrapper of memory resources as it would lead to undefined behavior when MemoryContext goes out of scope.

--- Syntropy/include/syntropy/allocators/memory_context.h
+++ Syntropy/include/syntropy/allocators/memory_context.h
@@ -6,10 +6,7 @@
 
 #pragma once
 
-#include "syntropy/language/type_traits.h"
-
 #include "syntropy/allocators/memory_resource.h"
-#include "syntropy/allocators/passthrough_memory_resource.h"
 
 namespace syntropy
 {
@@ -19,33 +16,24 @@ namespace syntropy
 
     /// \brief Represents a RAII guard to change a memory resource in the current scope and restore the previous one upon destruction.
     /// Memory contexts can be nested but overlapping results in undefined behavior.
-    ///
-    /// #TODO Syntropy memory resources are not polymorphic, hence the wrapper is necessary most of the times. However if TMemoryResource happens to be polymorphic,
-    ///       there's not need for the wrapper and the memory resource can be forwarded as-is and save some performance.
-    ///
     /// \author Raffaele D. Facendola - April 2020.
-    template <typename TMemoryResource>
-    class MemoryContextT
+    class MemoryContext
     {
     public:
 
         /// \brief Set a new memory resource for the current scope.
-        template <typename UMemoryResource>
-        MemoryContextT(UMemoryResource& memory_resource);
+        MemoryContext(MemoryResource& memory_resource);
 
         /// \brief Restore the previous memory resource.
-        ~MemoryContextT();
+        ~MemoryContext();
 
         /// \brief No copy constructor.
-        MemoryContextT(const MemoryContextT&) = delete;
+        MemoryContext(const MemoryContext&) = delete;
 
         /// \brief No assignment operator.
-        MemoryContextT& operator=(const MemoryContextT&) = delete;
+        MemoryContext& operator=(const MemoryContext&) = delete;
 
     public:
-
-        /// \brief Wrapper to a non-polymorphic memory resource type.
-        MemoryResourceT<PassthroughMemoryResource<TMemoryResource>> memory_resource_wrapper_;
 
         /// \brief Previous memory resource.
         MemoryResource* previous_memory_resource_ = nullptr;
@@ -53,40 +41,20 @@ namespace syntropy
     };
 
     /************************************************************************/
-    /* NON-MEMBER FUNCTIONS                                                 */
-    /************************************************************************/
-
-    /// \brief Create a new memory context deducing the type from the arguments.
-    template <typename TMemoryResource>
-    MemoryContextT<TMemoryResource> MakeMemoryContext(TMemoryResource& memory_resource);
-
-    /************************************************************************/
     /* IMPLEMENTATION                                                       */
     /************************************************************************/
 
-    // MemoryContext<TMemoryResource>.
+    // MemoryContext.
 
-    template <typename TMemoryResource>
-    template <typename UMemoryResource>
-    inline MemoryContextT<TMemoryResource>::MemoryContextT(UMemoryResource& memory_resource)
-        : memory_resource_wrapper_(memory_resource)
-        , previous_memory_resource_(&SetDefaultMemoryResource(memory_resource_wrapper_))
+    inline MemoryContext::MemoryContext(MemoryResource& memory_resource)
+        : previous_memory_resource_(&SetDefaultMemoryResource(memory_resource))
     {
 
     }
 
-    template <typename TMemoryResource>
-    inline MemoryContextT<TMemoryResource>::~MemoryContextT()
+    inline MemoryContext::~MemoryContext()
     {
         SetDefaultMemoryResource(*previous_memory_resource_);
-    }
-
-    // Non-member functions.
-
-    template <typename TMemoryResource>
-    inline MemoryContextT<TMemoryResource> MakeMemoryContext(TMemoryResource& memory_resource)
-    {
-        return MemoryContextT<TMemoryResource>(memory_resource);
     }
 
 }
