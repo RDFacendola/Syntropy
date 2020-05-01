10e137e0b7a4e0958039637977b8fa64ad63fdcb
Raffaele Daniele Facendola <raffaele.facendola@live.com>
2020-05-01 17:52:28 +0200

    Adding utility methods to create memory ranges.

--- Syntropy/include/syntropy/memory/memory_range.h
+++ Syntropy/include/syntropy/memory/memory_range.h
@@ -6,6 +6,8 @@
 
 #pragma once
 
+#include <type_traits>
+
 #include "syntropy/core/range.h"
 
 #include "syntropy/memory/memory_address.h"
@@ -16,7 +18,7 @@ namespace syntropy
     /* MEMORY RANGE                                                         */
     /************************************************************************/
 
-    /// \brief Type alias for a range of contiguous memory addresses.
+    /// \brief Type alias for a range of non-constant contiguous memory addresses.
     using MemoryRange = Range<MemoryAddress>;
 
     /// \brief Type alias for a range of constant contiguous memory addresses.
@@ -25,6 +27,26 @@ namespace syntropy
     /************************************************************************/
     /* NON-MEMBER FUNCTIONS                                                 */
     /************************************************************************/
+
+    /// \brief Create a new range from a pair of contiguous iterators.
+    template <typename TIterator>
+    auto MakeMemoryRange(TIterator begin, TIterator end);
+
+    /// \brief Create a new memory range from an iterator and a size.
+    template <typename TIterator>
+    auto MakeMemoryRange(TIterator begin, Bytes size);
+
+    /// \brief Create a new constant memory range from a pair of contiguous iterators.
+    template <typename TIterator>
+    ConstMemoryRange MakeConstMemoryRange(TIterator begin, TIterator end);
+
+    /// \brief Create a new constant memory range from an iterator and a size.
+    template <typename TIterator>
+    ConstMemoryRange MakeConstMemoryRange(TIterator begin, Bytes size);
+
+    /// \brief Create a memory range containing the provided object.
+    template <typename TCollection>
+    MemoryRange MakeMemoryRange(TCollection& data) noexcept;
 
     /// \brief Create a memory range containing the provided object.
     template <typename TType>
@@ -43,6 +65,54 @@ namespace syntropy
     /************************************************************************/
 
     // Non-member functions.
+
+    template <typename TIterator>
+    inline auto MakeMemoryRange(TIterator begin, TIterator end)
+    {
+        using std::distance;
+
+        // 'begin' and 'end' may not be dereferenced: use the size overload.
+
+        using TElement = std::remove_reference_t<decltype(*begin)>;
+
+        return MakeMemoryRange(begin, distance(begin, end) * BytesOf<TElement>());
+    }
+
+    template <typename TIterator>
+    inline auto MakeMemoryRange(TIterator begin, Bytes size)
+    {
+        using namespace literals;
+
+        // Compute range size since 'end' cannot be dereferenced.
+
+        auto address = (size > 0_Bytes) ? MakeMemoryAddress(&(*begin)) : nullptr;           // Either const or non-const.
+
+        return MakeRange(address, size);
+    }
+
+    template <typename TIterator>
+    inline ConstMemoryRange MakeConstMemoryRange(TIterator begin, TIterator end)
+    {
+        using std::distance;
+
+        // 'begin' and 'end' may not be dereferenced: use the size overload.
+
+        using TElement = std::remove_reference_t<decltype(*begin)>;
+
+        return MakeConstMemoryRange(begin, distance(begin, end) * BytesOf<TElement>());
+    }
+
+    template <typename TIterator>
+    inline ConstMemoryRange MakeConstMemoryRange(TIterator begin, Bytes size)
+    {
+        using namespace literals;
+
+        // Compute range size since 'end' cannot be dereferenced.
+
+        auto address = (size > 0_Bytes) ? MakeConstMemoryAddress(&(*begin)) : nullptr;      // Enforce const-ness.
+
+        return MakeConstRange(address, size);
+    }
 
     template <typename TType>
     inline MemoryRange MakeMemoryRange(TType& data) noexcept
