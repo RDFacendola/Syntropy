1ec30ce7206d5fe0963b3ba55873bc176b892b3a
Raffaele Daniele Facendola <raffaele.facendola@live.com>
2020-06-11 23:58:37 +0200

    Adding InitializerList type alias.

---
+++ Syntropy/include/syntropy/language/initializer_list.h
@@ -0,0 +1,21 @@
+
+/// \file initializer_list.h
+/// \brief This header is part of the Syntropy language module. It contains type aliases for initializer list.
+///
+/// \author Raffaele D. Facendola - 2017
+
+#pragma once
+
+#include <initializer_list>
+
+namespace syntropy
+{
+    /************************************************************************/
+    /* TYPE ALIAS                                                           */
+    /************************************************************************/
+
+    // Type alias for a lightweight proxy object that provides access to an array of objects of type const TElement.
+    template <typename TElement>
+    using InitializerList = std::initializer_list<TElement>;
+
+}
