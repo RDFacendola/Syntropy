1dd9a38b7912fbf76a6069a1d6409092272d4165
Raffaele Daniele Facendola <raffaele.facendola@live.com>
2020-06-28 19:28:16 +0200

    Slight documentation.

--- Syntropy/include/syntropy/unit_test/auto_test_case.h
+++ Syntropy/include/syntropy/unit_test/auto_test_case.h
@@ -108,7 +108,7 @@ namespace syntropy
     /************************************************************************/
 
     /// \brief Create an self-registering test case by deducing the fixture type from arguments.
-    /// \usage const auto my_test_case = MakeAutoTestCase(name, &MyFixture::Foo).
+    /// \usage const auto my_test_case = MakeAutoTestCase(name, LambdaTestCase).
     template <typename TTestFixture, typename TTestCase>
     AutoTestCaseT<TTestFixture, TTestCase> MakeAutoTestCase(const Label& name, TTestCase&& test_case);
 
