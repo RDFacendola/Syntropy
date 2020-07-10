fd989390426c43a0fca5c6bdc9fefb06dd4aee3e
Raffaele Daniele Facendola <raffaele.facendola@live.com>
2020-07-10 17:59:36 +0200

    Span is now fully constexpr \ noexcept.

--- Syntropy/include/syntropy/core/span.h
+++ Syntropy/include/syntropy/core/span.h
@@ -23,47 +23,43 @@ namespace syntropy
     public:
 
         /// \brief Create an empty span.
-        Span() = default;
+        constexpr Span() noexcept = default;
 
         /// \brief Create a span given an iterator to the first element and the span size.
         template <typename TBegin>
-        Span(TBegin begin, Int count);
+        constexpr Span(TBegin begin, Int count) noexcept;
 
         /// \brief Create a span given an iterator to the first element and an iterator past the last element.
         template <typename TBegin, typename TEnd, typename = EnableIfT<!IsConvertibleV<TEnd, Int>>>
-        Span(TBegin begin, TEnd end);
+        constexpr Span(TBegin begin, TEnd end) noexcept;
 
-        /// \brief Default copy constructor.
-        Span(const Span<TElement>& rhs) = default;
-
-        /// \brief Default move constructor.
-        Span(Span<TElement>&& rhs) = default;
+        /// \brief Copy constructor.
+        template <typename UElement>
+        constexpr Span(const Span<UElement>& rhs) noexcept;
 
         /// \brief Default destructor.
-        ~Span() = default;
+        ~Span() noexcept = default;
 
-        /// \brief Default copy assignment operator.
-        Span& operator=(const Span<TElement>& rhs) = default;
-
-        /// \brief Default move assignment operator.
-        Span& operator=(Span<TElement>&& rhs) = default;
+        /// \brief Copy assignment operator.
+        template <typename UElement>
+        constexpr Span& operator=(const Span<UElement>& rhs) noexcept;
 
         /// \brief Check whether the span is non-empty.
-        operator Bool() const;
+        constexpr operator Bool() const noexcept;
 
         /// \brief Access an element by index.
         /// If the provided index is not within the span the behavior of this method is undefined.
-        TElement& operator[](Int index) const;
+        constexpr TElement& operator[](Int index) const noexcept;
 
         /// \brief Get the number of elements in the span.
-        Int GetCount() const;
+        constexpr Int GetCount() const noexcept;
 
         /// \brief Access the underlying buffer or nullptr if the span is empty.
-        ObserverPtr<TElement> GetData() const;
+        constexpr ObserverPtr<TElement> GetData() const noexcept;
 
     private:
 
-        /// \brief Pointer to the first element in the rage.
+        /// \brief Pointer to the first element in the range.
         ObserverPtr<TElement> data_{ nullptr };
 
         /// \brief Number of elements in the span.
@@ -77,107 +73,107 @@ namespace syntropy
 
     /// \brief Get an iterator to the first element in a span.
     template <typename TElement>
-    inline ObserverPtr<TElement> begin(const Span<TElement>& span);
+    constexpr  ObserverPtr<TElement> begin(const Span<TElement>& span) noexcept;
 
     /// \brief Get an iterator past the last element in a span.
     template <typename TElement>
-    inline ObserverPtr<TElement> end(const Span<TElement>& span);
+    constexpr  ObserverPtr<TElement> end(const Span<TElement>& span) noexcept;
 
     /// \brief Check whether two spans are element-wise equivalent.
     template <typename TElement, typename UElement>
-    bool operator==(const Span<TElement>& lhs, const Span<UElement>& rhs);
+    constexpr bool operator==(const Span<TElement>& lhs, const Span<UElement>& rhs) noexcept;
 
     /// \brief Check whether two spans are not element-wise equivalent.
     template <typename TElement, typename UElement>
-    bool operator!=(const Span<TElement>& lhs, const Span<UElement>& rhs);
+    constexpr bool operator!=(const Span<TElement>& lhs, const Span<UElement>& rhs) noexcept;
 
     /// \brief Check whether a span is empty.
     template <typename TElement>
-    Bool IsEmpty(const Span<TElement>& span);
+    constexpr Bool IsEmpty(const Span<TElement>& span) noexcept;
 
     /// \brief Get the number of elements in a span.
     template <typename TElement>
-    Int Count(const Span<TElement>& span);
+    constexpr Int Count(const Span<TElement>& span) noexcept;
 
     /// \brief Access the first element in a span.
     /// If the span is empty the behavior of this method is undefined.
     template <typename TElement>
-    TElement& Front(const Span<TElement>& span);
+    constexpr TElement& Front(const Span<TElement>& span) noexcept;
 
     /// \brief Access the last element in a span.
     /// If the span is empty the behavior of this method is undefined.
     template <typename TElement>
-    TElement& Back(const Span<TElement>& span);
+    constexpr TElement& Back(const Span<TElement>& span) noexcept;
 
     /// \brief Obtain a sub-span given an offset and a number of elements.
     /// If the sub-span would exceed original span bounds, the behavior of this method is undefined.
     template <typename TElement>
-    Span<TElement> Subspan(const Span<TElement>& span, Int offset, Int count);
+    constexpr Span<TElement> Subspan(const Span<TElement>& span, Int offset, Int count) noexcept;
 
     /// \brief Discard the first element in a span and return the resulting subspan.
     /// If the span is empty the behavior of this method is undefined.
     template <typename TElement>
-    Span<TElement> PopFront(const Span<TElement>& span);
+    constexpr Span<TElement> PopFront(const Span<TElement>& span) noexcept;
 
     /// \brief Discard the first count-elements in a span and return the resulting subspan.
     /// If this method would cause the subspan to exceed the original span, the behavior of this method is undefined.
     template <typename TElement>
-    Span<TElement> PopFront(const Span<TElement>& span, Int count);
+    constexpr Span<TElement> PopFront(const Span<TElement>& span, Int count) noexcept;
 
     /// \brief Discard the last element in a span and return the resulting subspan.
     /// If the span is empty the behavior of this method is undefined.
     template <typename TElement>
-    Span<TElement> PopBack(const Span<TElement>& span);
+    constexpr Span<TElement> PopBack(const Span<TElement>& span) noexcept;
 
     /// \brief Discard the last count-elements in a span and return the resulting subspan.
     /// If this method would cause the subspan to exceed the original span, the behavior of this method is undefined.
     template <typename TElement>
-    Span<TElement> PopBack(const Span<TElement>& span, Int count);
+    constexpr Span<TElement> PopBack(const Span<TElement>& span, Int count) noexcept;
 
     /// \brief Obtain a span consisting of the first elements of another span.
     /// If this method would cause the subspan to exceed the original span, the behavior of this method is undefined.
     template <typename TElement>
-    Span<TElement> First(const Span<TElement>& span, Int count);
+    constexpr Span<TElement> First(const Span<TElement>& span, Int count) noexcept;
 
     /// \brief Obtain a span consisting of the last elements of another span.
     /// If this method would cause the subspan to exceed the original span, the behavior of this method is undefined.
     template <typename TElement>
-    Span<TElement> Last(const Span<TElement>& span, Int count);
+    constexpr Span<TElement> Last(const Span<TElement>& span, Int count) noexcept;
 
     /// \brief Check whether two spans are identical (ie: they both refer to the same memory).
-    template <typename TElement>
-    Bool AreIdentical(const Span<TElement>& lhs, const Span<TElement>& rhs);
+    template <typename TElement, typename UElement>
+    constexpr Bool AreIdentical(const Span<TElement>& lhs, const Span<UElement>& rhs) noexcept;
 
     /// \brief Check whether two spans are element-wise equivalent.
     template <typename TElement, typename UElement>
-    Bool AreEquivalent(const Span<TElement>& lhs, const Span<UElement>& rhs);
+    constexpr Bool AreEquivalent(const Span<TElement>& lhs, const Span<UElement>& rhs) noexcept;
 
     /// \brief Check whether rhs is a a member-wise prefix of lhs.
     template <typename TElement, typename UElement>
-    Bool HasPrefix(const Span<TElement>& lhs, const Span<UElement>& rhs);
+    constexpr Bool HasPrefix(const Span<TElement>& lhs, const Span<UElement>& rhs) noexcept;
 
     /// \brief Check whether rhs is a a member-wise suffix of lhs.
     template <typename TElement, typename UElement>
-    Bool HasSuffix(const Span<TElement>& lhs, const Span<UElement>& rhs);
+    constexpr Bool HasSuffix(const Span<TElement>& lhs, const Span<UElement>& rhs) noexcept;
 
     /// \brief Check whether exists a subspan in lhs which compares equivalent to rhs.
     /// \return Returns true if lhs has a subset which compares equivalent to rhs, returns false otherwise.
     template <typename TElement, typename UElement>
-    Bool HasSubspan(const Span<TElement>& lhs, const Span<UElement>& rhs);
+    constexpr Bool HasSubspan(const Span<TElement>& lhs, const Span<UElement>& rhs) noexcept;
 
     /// \brief Reduce lhs until rhs becomes a member-wise prefix for lhs or lhs is exhausted.
     /// \return Returns the reduced range starting from the first occurrence of rhs in lhs or an empty range if no occurrence was found.
     template <typename TElement, typename UElement>
-    Span<TElement> Search(const Span<TElement>& lhs, const Span<UElement>& rhs);
+    constexpr Span<TElement> Search(const Span<TElement>& lhs, const Span<UElement>& rhs) noexcept;
 
     /// \brief Check whether rhs refers to a memory location identical or contained inside lhs.
-    template <typename TElement>
-    Bool Contains(const Span<TElement>& lhs, const Span<TElement>& rhs);
+    template <typename TElement, typename UElement>
+    constexpr Bool Contains(const Span<TElement>& lhs, const Span<UElement>& rhs) noexcept;
 
     /// \brief Check whether lhs and lhs overlaps the same memory location.
     /// Empty spans are not considered to be overlapping with any other span.
-    template <typename TElement>
-    Bool IsOverlapping(const Span<TElement>& lhs, const Span<TElement>& rhs);
+    template <typename TElement, typename UElement>
+    constexpr Bool IsOverlapping(const Span<TElement>& lhs, const Span<UElement>& rhs) noexcept;
 
     /************************************************************************/
     /* IMPLEMENTATION                                                       */
@@ -187,7 +183,7 @@ namespace syntropy
 
     template <typename TElement>
     template <typename TBegin>
-    inline Span<TElement>::Span(TBegin begin, Int count)
+    constexpr Span<TElement>::Span(TBegin begin, Int count) noexcept
         : data_((count > 0) ? &(*begin) : nullptr)
         , count_(count)
     {
@@ -196,44 +192,63 @@ namespace syntropy
 
     template <typename TElement>
     template <typename TBegin, typename TEnd, typename>
-    inline Span<TElement>::Span(TBegin begin, TEnd end)
+    constexpr Span<TElement>::Span(TBegin begin, TEnd end) noexcept
         : Span(begin, ToInt(end - begin))
     {
 
     }
 
     template <typename TElement>
-    inline Span<TElement>::operator Bool() const
+    template <typename UElement>
+    constexpr Span<TElement>::Span(const Span<UElement>& rhs) noexcept
+        : data_(rhs.data_)
+        , count_(rhs.count_)
+    {
+
+    }
+
+    template <typename TElement>
+    template <typename UElement>
+    constexpr Span<TElement>& Span<TElement>::operator=(const Span<UElement>& rhs) noexcept
+    {
+        data_ = rhs.data_;
+        count_ = rhs.count_;
+
+        return *this;
+    }
+
+    template <typename TElement>
+    constexpr Span<TElement>::operator Bool() const noexcept
     {
         return count_ > 0;
     }
 
     template <typename TElement>
-    inline  TElement& Span<TElement>::operator[](Int index) const
+    constexpr TElement& Span<TElement>::operator[](Int index) const noexcept
     {
         return data_[index];
     }
 
     template <typename TElement>
-    inline Int Span<TElement>::GetCount() const
+    constexpr Int Span<TElement>::GetCount() const noexcept
     {
         return count_;
     }
 
     template <typename TElement>
-    inline ObserverPtr<TElement> Span<TElement>::GetData() const
+    constexpr ObserverPtr<TElement> Span<TElement>::GetData() const noexcept
     {
         return data_;
     }
 
     template <typename TElement>
-    inline ObserverPtr<TElement> begin(const Span<TElement>& span)
+    constexpr ObserverPtr<TElement> begin(const Span<TElement>& span) noexcept
     {
         return span.GetData();
     }
 
     template <typename TElement>
-    inline ObserverPtr<TElement> end(const Span<TElement>& span)
+    constexpr ObserverPtr<TElement> end(const Span<TElement>& span) noexcept
     {
         return span.GetData() + span.count_;
     }
@@ -241,91 +256,91 @@ namespace syntropy
     // Non-member functions.
 
     template <typename TElement, typename UElement>
-    inline bool operator==(const Span<TElement>& lhs, const Span<UElement>& rhs)
+    constexpr bool operator==(const Span<TElement>& lhs, const Span<UElement>& rhs) noexcept
     {
         return AreEquivalent(lhs, rhs);
     }
 
     template <typename TElement, typename UElement>
-    inline bool operator!=(const Span<TElement>& lhs, const Span<UElement>& rhs)
+    constexpr bool operator!=(const Span<TElement>& lhs, const Span<UElement>& rhs) noexcept
     {
         return !(lhs == rhs);
     }
 
     template <typename TElement>
-    inline Bool IsEmpty(const Span<TElement>& span)
+    constexpr Bool IsEmpty(const Span<TElement>& span) noexcept
     {
         return !span;
     }
 
     template <typename TElement>
-    inline Int Count(const Span<TElement>& span)
+    constexpr Int Count(const Span<TElement>& span) noexcept
     {
         return span.GetCount();
     }
 
     template <typename TElement>
-    inline TElement& Front(const Span<TElement>& span)
+    constexpr TElement& Front(const Span<TElement>& span) noexcept
     {
         return span[0];
     }
 
     template <typename TElement>
-    inline TElement& Back(const Span<TElement>& span)
+    constexpr TElement& Back(const Span<TElement>& span) noexcept
     {
         return span[span.GetCount() - 1];
     }
 
     template <typename TElement>
-    inline Span<TElement> Subspan(const Span<TElement>& span, Int offset, Int count)
+    constexpr Span<TElement> Subspan(const Span<TElement>& span, Int offset, Int count) noexcept
     {
         return { &span[offset], count };
     }
 
     template <typename TElement>
-    inline Span<TElement> PopFront(const Span<TElement>& span)
+    constexpr Span<TElement> PopFront(const Span<TElement>& span) noexcept
     {
         return PopFront(span, 1);
     }
 
     template <typename TElement>
-    inline Span<TElement> PopFront(const Span<TElement>& span, Int count)
+    constexpr Span<TElement> PopFront(const Span<TElement>& span, Int count) noexcept
     {
         return { &span[count], span.GetCount() - count };
     }
 
     template <typename TElement>
-    inline Span<TElement> PopBack(const Span<TElement>& span)
+    constexpr Span<TElement> PopBack(const Span<TElement>& span) noexcept
     {
         return PopBack(span, 1);
     }
 
     template <typename TElement>
-    inline Span<TElement> PopBack(const Span<TElement>& span, Int count)
+    constexpr Span<TElement> PopBack(const Span<TElement>& span, Int count) noexcept
     {
         return { &span[0], span.GetCount() - count };
     }
 
     template <typename TElement>
-    inline Span<TElement> First(const Span<TElement>& span, Int count)
+    constexpr Span<TElement> First(const Span<TElement>& span, Int count) noexcept
     {
         return { &span[0], count };
     }
 
     template <typename TElement>
-    inline Span<TElement> Last(const Span<TElement>& span, Int count)
+    constexpr Span<TElement> Last(const Span<TElement>& span, Int count) noexcept
     {
         return { &span[span.GetCount() - count], count };
     }
 
-    template <typename TElement>
-    inline Bool AreIdentical(const Span<TElement>& lhs, const Span<TElement>& rhs)
+    template <typename TElement, typename UElement>
+    constexpr Bool AreIdentical(const Span<TElement>& lhs, const Span<UElement>& rhs) noexcept
     {
         return (lhs.GetData() == rhs.GetData()) && (lhs.GetCount() == rhs.GetCount());
     }
 
     template <typename TElement, typename UElement>
-    Bool AreEquivalent(const Span<TElement>& lhs, const Span<UElement>& rhs)
+    constexpr Bool AreEquivalent(const Span<TElement>& lhs, const Span<UElement>& rhs) noexcept
     {
         // Early out if the number of elements is different.
 
@@ -339,7 +354,7 @@ namespace syntropy
         auto lhs_data = lhs.GetData();
         auto rhs_data = rhs.GetData();
 
-        if constexpr (IsSameV<TElement, UElement>)
+        if constexpr (IsSameV<std::decay<TElement>, std::decay<UElement>>)
         {
             if (lhs_data == rhs_data)
             {
@@ -361,7 +376,7 @@ namespace syntropy
     }
 
     template <typename TElement, typename UElement>
-    inline Bool HasPrefix(const Span<TElement>& lhs, const Span<UElement>& rhs)
+    constexpr Bool HasPrefix(const Span<TElement>& lhs, const Span<UElement>& rhs) noexcept
     {
         auto lhs_count = Count(lhs);
         auto rhs_count = Count(rhs);
@@ -370,7 +385,7 @@ namespace syntropy
     }
 
     template <typename TElement, typename UElement>
-    inline Bool HasSuffix(const Span<TElement>& lhs, const Span<UElement>& rhs)
+    constexpr Bool HasSuffix(const Span<TElement>& lhs, const Span<UElement>& rhs) noexcept
     {
         auto lhs_count = Count(lhs);
         auto rhs_count = Count(rhs);
@@ -379,13 +394,13 @@ namespace syntropy
     }
 
     template <typename TElement, typename UElement>
-    inline Bool HasSubspan(const Span<TElement>& lhs, const Span<UElement>& rhs)
+    constexpr Bool HasSubspan(const Span<TElement>& lhs, const Span<UElement>& rhs) noexcept
     {
         return Count(Search(lhs, rhs)) >= Count(rhs);
     }
 
     template <typename TElement, typename UElement>
-    inline Span<TElement> Search(const Span<TElement>& lhs, const Span<UElement>& rhs)
+    constexpr Span<TElement> Search(const Span<TElement>& lhs, const Span<UElement>& rhs) noexcept
     {
         auto result = lhs;
 
@@ -394,9 +409,11 @@ namespace syntropy
         return result;
     }
 
-    template <typename TElement>
-    inline Bool Contains(const Span<TElement>& lhs, const Span<TElement>& rhs)
+    template <typename TElement, typename UElement>
+    constexpr Bool Contains(const Span<TElement>& lhs, const Span<UElement>& rhs) noexcept
     {
+        static_assert(IsSameV<std::decay<TElement>, std::decay<UElement>>, "TElement and UElement must refer to the same underlying type");
+
         auto lhs_begin = lhs.GetData();
         auto lhs_end = lhs_begin + Count(lhs);
 
@@ -406,9 +423,11 @@ namespace syntropy
         return (!rhs) || ((lhs_begin <= rhs_begin) && (lhs_end >= rhs_end));
     }
 
-    template <typename TElement>
-    inline Bool IsOverlapping(const Span<TElement>& lhs, const Span<TElement>& rhs)
+    template <typename TElement, typename UElement>
+    constexpr Bool IsOverlapping(const Span<TElement>& lhs, const Span<UElement>& rhs) noexcept
     {
+        static_assert(IsSameV<std::decay<TElement>, std::decay<UElement>>, "TElement and UElement must refer to the same underlying type");
+
         auto lhs_begin = lhs.GetData();
         auto lhs_end = lhs_begin + Count(lhs);
 
