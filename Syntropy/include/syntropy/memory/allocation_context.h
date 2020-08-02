ae8702d780cf8ea17c60afb79c9ec8ef15fbad8c
Raffaele Daniele Facendola <raffaele.facendola@live.com>
2020-08-02 23:27:49 +0200

    Renaming InlineMemoryContext as InlineAllocationContext and moving the class under syntropy/allocation_context.h.

--- Syntropy/include/syntropy/memory/allocation_context.h
+++ Syntropy/include/syntropy/memory/allocation_context.h
@@ -22,10 +22,10 @@ namespace syntropy
     {
     public:
 
-        /// \brief Set a new memory resource for the current scope.
+        /// \brief Set a new allocator for the current scope.
         AllocationContext(Allocator& allocator) noexcept;
 
-        /// \brief Restore the previous memory resource.
+        /// \brief Restore the previous allocator.
         ~AllocationContext() noexcept;
 
         /// \brief No copy constructor.
@@ -38,6 +38,39 @@ namespace syntropy
 
         /// \brief Previous allocator.
         Pointer<Allocator> previous_allocator_{ nullptr };
+
+    };
+
+    /************************************************************************/
+    /* INLINE ALLOCATION CONTEXT                                            */
+    /************************************************************************/
+
+    /// \brief Represents a RAII guard to change the allocator in the current scope and restore the previous one upon destruction.
+    /// Inline contexts wrap their own allocator: when the allocator goes out of scope accessing the allocated blocks results in undefined behavior.
+    /// Memory contexts can be nested but overlapping results in undefined behavior.
+    /// \author Raffaele D. Facendola - May 2020.
+    template <typename TAllocator>
+    class InlineAllocationContext : public AllocationContext
+    {
+    public:
+
+        /// \brief Set a new allocator for the current scope.
+        template <typename... TArguments>
+        InlineAllocationContext(TArguments&&... arguments) noexcept;
+
+        /// \brief Restore the previous allocator.
+        ~InlineAllocationContext() = default;
+
+        /// \brief No copy constructor.
+        InlineAllocationContext(const InlineAllocationContext&) = delete;
+
+        /// \brief No assignment operator.
+        InlineAllocationContext& operator=(const InlineAllocationContext&) = delete;
+
+    public:
+
+        /// \brief Underlying allocator.
+        AllocatorT<TAllocator> allocator_;
 
     };
 
@@ -57,6 +90,18 @@ namespace syntropy
     inline AllocationContext::~AllocationContext() noexcept
     {
         Memory::SetAllocator(*previous_allocator_);
+    }
+
+    // InlineAllocationContext<TAllocator>.
+    // ====================================
+
+    template <typename TAllocator>
+    template <typename... TArguments>
+    inline InlineAllocationContext<TAllocator>::InlineAllocationContext(TArguments&&... arguments) noexcept
+        : AllocationContext(&allocator_)
+        , allocator_(std::forward<TArguments>(arguments)...)
+    {
+
     }
 
 }
