0da33f84cb2c32bd4b873397bbd0c39335b9b53b
Raffaele Daniele Facendola <raffaele.facendola@live.com>
2020-08-09 23:52:48 +0200

    Adding Any, All and None predicates to iterable collections.

--- Syntropy/include/syntropy/language/algorithm.h
+++ Syntropy/include/syntropy/language/algorithm.h
@@ -9,6 +9,7 @@
 #include <initializer_list>
 #include <algorithm>
 
+#include "syntropy/language/utility.h"
 #include "syntropy/memory/byte_span.h"
 
 namespace syntropy
@@ -20,16 +21,65 @@ namespace syntropy
     /// \brief Rotate the order of the element in a range, in such a way that the element pointed by middle becomes the new first element.
     /// If middle doesn't belong to range, the result of this method is undefined.
     template <typename TElement>
-    void Rotate(const RWSpan<TElement>& span, Pointer<TElement> middle);
+    void Rotate(const RWSpan<TElement>& span, Pointer<TElement> middle) noexcept;
+
+    /// \brief Check whether a predicate holds true for at least one element in a collection.
+    template <typename TIterable, typename TPredicate>
+    Bool Any(const TIterable& iterable, TPredicate&& predicate) noexcept;
+
+    /// \brief Check whether a predicate holds true for all the elements in a collection.
+    template <typename TIterable, typename TPredicate>
+    Bool All(const TIterable& iterable, TPredicate&& predicate) noexcept;
+
+    /// \brief Check whether a predicate holds false for all the elements in a collection.
+    template <typename TIterable, typename TPredicate>
+    Bool None(const TIterable& iterable, TPredicate&& predicate) noexcept;
 
     /************************************************************************/
     /* IMPLEMENTATION                                                       */
     /************************************************************************/
 
+    // Non-member functions.
+    // =====================
+
     template <typename TElement>
-    inline void Rotate(const RWSpan<TElement>& span, Pointer<TElement> middle)
+    inline void Rotate(const RWSpan<TElement>& span, Pointer<TElement> middle) noexcept
     {
         std::rotate(Begin(span), middle, End(span));
+    }
+
+    template <typename TIterable, typename TPredicate>
+    inline Bool Any(const TIterable& rhs, TPredicate&& predicate) noexcept
+    {
+        for (auto&& element : rhs)
+        {
+            if (predicate(ReadOnly(element)))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    template <typename TIterable, typename TPredicate>
+    inline Bool All(const TIterable& rhs, TPredicate&& predicate) noexcept
+    {
+        for (auto&& element : rhs)
+        {
+            if (!predicate(ReadOnly(element)))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    template <typename TIterable, typename TPredicate>
+    inline Bool None(const TIterable& rhs, TPredicate&& predicate) noexcept
+    {
+        return !Any(rhs, std::forward<TPredicate>(predicate));
     }
 
 }
