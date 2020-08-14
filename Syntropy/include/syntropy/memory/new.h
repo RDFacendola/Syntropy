89c2af8744cefa5139d6e7f1f92acf10854ea6e5
Raffaele Daniele Facendola <raffaele.facendola@live.com>
2020-08-14 07:16:12 +0200

    Adding placement new for RW byte spans.

---
+++ Syntropy/include/syntropy/memory/new.h
@@ -0,0 +1,36 @@
+
+/// \file new.h
+/// \brief This header is part of the Syntropy memory module. It contains custom variants of both new\delete operators.
+///
+/// \author Raffaele D. Facendola - 2017
+
+#pragma once
+
+#include "syntropy/core/types.h"
+#include "syntropy/memory/byte_span.h"
+
+/************************************************************************/
+/* OPERATOR NEW                                                         */
+/************************************************************************/
+
+/// \brief Allocate requested number of bytes inside byte span.
+/// If the byte span could not fit the requested size, returns nullptr.
+Syntropy::RWTypelessPtr operator new(std::size_t size, const Syntropy::RWByteSpan& byte_span);
+
+/************************************************************************/
+/* IMPLEMENTATION                                                       */
+/************************************************************************/
+
+// Operator New.
+// =============
+
+inline Syntropy::RWTypelessPtr operator new(std::size_t size, const Syntropy::RWByteSpan& byte_span)
+{
+    if (size <= Syntropy::ToInt(Syntropy::Memory::Size(byte_span)))
+    {
+        return Begin(byte_span);
+    }
+
+    return nullptr;
+}
+
