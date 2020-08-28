3b0236328c5773013720d2f5f64b76ab43528a7f
Raffaele Daniele Facendola <raffaele.facendola@live.com>
2020-08-28 07:54:04 +0200

    Removing unnecessary type aliases for both references and pointers.

--- Syntropy/include/syntropy/language/foundation/pointers.h
+++ Syntropy/include/syntropy/language/foundation/pointers.h
@@ -19,17 +19,20 @@ namespace Syntropy
     /************************************************************************/
 
     /// \brief Type alias for a pointer to a *read-only* pointee. This pointer doesn't participate to pointee life-time.
-    /// Binds to both read-only and read-write pointers.
+    /// This alias may be used for declaring variables with the syntax: auto p = Pointer<...>{ nullptr }.
+    /// \remarks This alias is expected not to be used with templates as it will clutter syntax.
     template <typename TType>
     using Pointer = const TType*;
 
     /// \brief Type alias for a pointer to a *read-write* pointee. This pointer doesn't participate to pointee life-time.
-    /// TType must be non-constant (aka read-write).
+    /// This alias may be used for declaring variables with the syntax: auto p = RWPointer<...>{ nullptr }.
+    /// \remarks This alias is expected not to be used with templates as it will clutter syntax.
     template <typename TType, typename = Traits::EnableIf<!Traits::IsConst<TType>>>
     using RWPointer = TType*;
 
-    /// \brief Type alias for a pointer to either a *read-only* or *read-write* pointee. This Pointer doesn't participate to pointee life-time.
-    /// Depending on TType, this type is either equal to Pointer<TType or to RWPointer<T>.
+    /// \brief Type alias for a pointer to either a *read-write* or *read-only* pointee. This pointer doesn't participate to pointee life-time.
+    /// This alias may be used for declaring variables with the syntax: auto p = XPointer<...>{ nullptr }.
+    /// \remarks This alias is expected not to be used with templates as it will clutter syntax.
     template <typename TType>
     using XPointer = TType*;
 
@@ -44,25 +47,33 @@ namespace Syntropy
     /* NON-MEMBER FUNCTIONS                                                 */
     /************************************************************************/
 
+    // Utilities.
+
     /// \brief Convert rhs to a pointer to UType.
     /// \remarks If the pointee type is not related to UType, the program is ill-formed.
     template <typename TType, typename UType>
-    constexpr XPointer<TType> ToPointer(XPointer<UType> rhs) noexcept;
+    [[nodiscard]] constexpr TType* ToPointer(UType* rhs) noexcept;
 
     /// \brief Obtains the actual address of rhs, even in presence of overloaded operator&.
     template <typename TType>
-    constexpr XPointer<TType> AddressOf(XReference<TType> rhs) noexcept;
+    [[nodiscard]] constexpr TType* AddressOf(TType& rhs) noexcept;
+
+    /// \brief Rvalue references have no address.
+    template <typename TType>
+    [[nodiscard]] constexpr void AddressOf(const TType&& rhs) noexcept = delete;
+
+    // Read-only / Read-write.
 
      /// \brief Convert rhs to a pointer to a read-only value.
     template <typename TType>
-    [[nodiscard]] constexpr Pointer<TType> ReadOnly(Pointer<TType> rhs) noexcept;
+    [[nodiscard]] constexpr const TType* ReadOnly(const TType* rhs) noexcept;
 
     /// \brief Convert rhs to a pointer to a read-write object.
     /// The intended use for this method is to write a non-const implementation based on a const implementation, without duplicating associated code.
-    /// Such usage has the form: ReadWrite(F(ReadOnly(x))), x is non-const.
+    /// Such usage has the form: ReadWrite(F(ReadOnly(x))) where x is non-const and F(.) is a function.
     /// \remarks If rhs pointee doesn't refer to a read-write object, accessing the result of this method results in undefined behavior.
     template <typename TType>
-    [[nodiscard]] constexpr RWPointer<TType> ReadWrite(Pointer<TType> rhs) noexcept;
+    [[nodiscard]] constexpr TType* ReadWrite(const TType* rhs) noexcept;
 
     /************************************************************************/
     /* IMPLEMENTATION                                                       */
@@ -71,28 +82,32 @@ namespace Syntropy
     // Non-member functions.
     // =====================
 
+    // Utilities.
+
     template <typename TType, typename UType>
-    constexpr XPointer<TType> ToPointer(XPointer<UType> rhs) noexcept
+    constexpr TType* ToPointer(UType* rhs) noexcept
     {
-        return static_cast<XPointer<TType>>(rhs);
+        return static_cast<TType*>(rhs);
     }
 
     template <typename TType>
-    constexpr XPointer<TType> AddressOf(XReference<TType> rhs) noexcept
+    constexpr TType* AddressOf(TType& rhs) noexcept
     {
         return std::addressof(rhs);
     }
 
+    // Read-only / Read-write.
+
     template <typename TType>
-    constexpr Pointer<TType> ReadOnly(Pointer<TType> rhs) noexcept
+    constexpr const TType* ReadOnly(TType* rhs) noexcept
     {
         return rhs;
     }
 
     template <typename TType>
-    constexpr RWPointer<TType> ReadWrite(Pointer<TType> rhs) noexcept
+    constexpr TType* ReadWrite(const TType* rhs) noexcept
     {
-        return const_cast<RWPointer<TType>>(rhs);
+        return const_cast<TType*>(rhs);
     }
 
 }
--- Syntropy/include/syntropy/language/foundation/references.h
+++ Syntropy/include/syntropy/language/foundation/references.h
@@ -21,47 +21,25 @@ namespace Syntropy
     /************************************************************************/
 
     /// \brief Type alias for a lvalue reference to a *read-only* object.
+    /// \remarks This alias is expected not to be used with templates as it will clutter syntax.
     template <typename TType>
     using Reference = const TType&;
 
     /// \brief Type alias for a lvalue reference to a *read-write* object.
+    /// \remarks This alias is expected not to be used with templates as it will clutter syntax.
     template <typename TType, typename = Traits::EnableIf<!Traits::IsConst<Traits::RemoveReference<TType&>>>>
     using RWReference = TType&;
 
-    /// \brief Type alias for a lvalue reference to either a *read-only* or *read-write* object.
+    /// \brief Type alias for a lvalue reference to either a *read-write* or *read-only* object.
+    /// \remarks This alias is expected not to be used with templates as it will clutter syntax.
     template <typename TType>
     using XReference = TType&;
 
     /************************************************************************/
-    /* TRANSIENT                                                            */
+    /* UTILITIES                                                             */
     /************************************************************************/
 
-    /// \brief Type alias for a rvalue reference to a *read-only* object.
-    template <typename TType, typename = Traits::EnableIf<Traits::IsRValueReference<const TType&&>>>
-    using Transient = const TType&&;
-
-    /// \brief Type alias for a rvalue reference to a *read-write* object.
-    template <typename TType, typename = Traits::EnableIf<Traits::IsRValueReference<TType&&> && !Traits::IsConst<Traits::RemoveReference<TType&>>>>
-    using RWTransient = TType&&;
-
-    /// \brief Type alias for a rvalue reference to either a *read-only* or *read-write* object.
-    template <typename TType, typename = Traits::EnableIf<Traits::IsRValueReference<TType&&>>>
-    using XTransient = TType&&;
-
-    /************************************************************************/
-    /* FORWARDING                                                           */
-    /************************************************************************/
-
-    /// \brief Type alias for a forwarding reference (aka universal reference).
-    /// Note that this declares the *intent* of using TType as forwarding reference, it does not *enforce* it.
-    /// This type behaves as either a XReference or a XTransient.
-    /// TType is expected to be bound to a template argument.
-    template <typename TType>
-    using Forwarding = TType&&;
-
-    /************************************************************************/
-    /* NON-MEMBER FUNCTIONS                                                 */
-    /************************************************************************/
+    // Move \ Forward.
 
     /// \brief Indicate that rhs may be "moved from", allowing for efficient transfer of resources from rhs to another object.
     template <typename TType>
@@ -75,31 +53,35 @@ namespace Syntropy
     template <typename TType>
     [[nodiscard]] constexpr TType&& Forward(Traits::RemoveReference<TType>&& rhs) noexcept;
 
+    // Read-only \ Read-write.
+
     /// \brief Convert rhs to a read-only reference.
     template <typename TType>
-    [[nodiscard]] constexpr Reference<TType> ReadOnly(Reference<TType> rhs) noexcept;
+    [[nodiscard]] constexpr const TType& ReadOnly(Reference<TType> rhs) noexcept;
 
     /// \brief Rvalues shall not be converted to read-only.
     template <typename TType>
-    [[nodiscard]] constexpr void ReadOnly(Transient<TType> rhs) noexcept = delete;
+    [[nodiscard]] constexpr void ReadOnly(const TType&& rhs) noexcept = delete;
 
     /// \brief Convert rhs to a read-write reference.
     /// The intended use for this method is to write a non-const implementation based on a const implementation, without duplicating code.
-    /// Such usage has the form: ReadWrite(F(ReadOnly(x))), x is non-const.
+    /// Such usage has the form: ReadWrite(F(ReadOnly(x))) where x is non-const and F(.) is a function.
     /// \remarks If rhs doesn't refer to a read-write object, accessing the result of this method results in undefined behavior.
     template <typename TType>
-    [[nodiscard]] constexpr RWReference<TType> ReadWrite(Reference<TType> rhs) noexcept;
+    [[nodiscard]] constexpr TType& ReadWrite(Reference<TType> rhs) noexcept;
 
     /// \brief Rvalues shall not be converted to read-write.
     template <typename TType>
-    [[nodiscard]] constexpr RWTransient<TType> ReadWrite(Transient<TType> rhs) noexcept = delete;
+    [[nodiscard]] constexpr void ReadWrite(const TType&& rhs) noexcept = delete;
 
     /************************************************************************/
     /* IMPLEMENTATION                                                       */
     /************************************************************************/
 
-    // Non-member functions.
-    // =====================
+    // Utilities.
+    // =========
+
+    // Move \ Forward.
 
     template <typename TType>
     constexpr Traits::RemoveReference<TType>&& Move(TType&& rhs) noexcept
@@ -119,6 +101,8 @@ namespace Syntropy
         return static_cast<TType&&>(rhs);
     }
 
+    // Read-only \ Read-write.
+
     template <typename TType>
     constexpr Reference<TType> ReadOnly(Reference<TType> rhs) noexcept
     {
@@ -128,7 +112,7 @@ namespace Syntropy
     template <typename TType>
     constexpr RWReference<TType> ReadWrite(Reference<TType> rhs) noexcept
     {
-        return const_cast<RWReference<TType>>(rhs);
+        return const_cast<TType&>(rhs);
     }
 
 }
