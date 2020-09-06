c38a5f70257d4f45b9030261f98c10417169c99f
Raffaele Daniele Facendola <raffaele.facendola@live.com>
2020-09-06 19:18:36 +0200

    Further cleaning up of rational unit tests.

--- test/include/unit_tests/syntropy/language/templates/rational_unit_test.h
+++ test/include/unit_tests/syntropy/language/templates/rational_unit_test.h
@@ -25,112 +25,112 @@ namespace Syntropy::Templates::UnitTest
 
     .TestCase("Adding two rationals together returns the unreduced sum of the twos.", [](auto& fixture)
     {
-        using sum_3over6_2over5 = RationalSum<Rational<3, 6>, Rational<2, 5>>;
+        using Sum_3o6_2o5 = RationalSum<Rational<3, 6>, Rational<2, 5>>;
 
-        SYNTROPY_UNIT_EQUAL(sum_3over6_2over5::kNumerator, 27);
-        SYNTROPY_UNIT_EQUAL(sum_3over6_2over5::kDenominator, 30);
+        SYNTROPY_UNIT_EQUAL(Sum_3o6_2o5::kNumerator, 27);
+        SYNTROPY_UNIT_EQUAL(Sum_3o6_2o5::kDenominator, 30);
     })
 
     .TestCase("Adding zero to a rational returns a rational is equivalent to the latter.", [](auto& fixture)
     {
-        using sum_3over6_0over1 = RationalSum<Rational<3, 6>, Rational<0, 1>>;
+        using Sum_3o6_0o1 = RationalSum<Rational<3, 6>, Rational<0, 1>>;
 
-        SYNTROPY_UNIT_EQUAL(sum_3over6_0over1::kNumerator, 3);
-        SYNTROPY_UNIT_EQUAL(sum_3over6_0over1::kDenominator, 6);
+        SYNTROPY_UNIT_EQUAL(Sum_3o6_0o1::kNumerator, 3);
+        SYNTROPY_UNIT_EQUAL(Sum_3o6_0o1::kDenominator, 6);
 
-        using sum_0over6_3over1 = RationalSum<Rational<0, 6>, Rational<3, 1>>;
+        using Sum_0o6_3o1 = RationalSum<Rational<0, 6>, Rational<3, 1>>;
 
-        SYNTROPY_UNIT_EQUAL(sum_0over6_3over1::kNumerator, 18);
-        SYNTROPY_UNIT_EQUAL(sum_0over6_3over1::kDenominator, 6);
+        SYNTROPY_UNIT_EQUAL(Sum_0o6_3o1::kNumerator, 18);
+        SYNTROPY_UNIT_EQUAL(Sum_0o6_3o1::kDenominator, 6);
     })
 
     .TestCase("Subtracting a rational from another rational returns the unreduced difference of the twos.", [](auto& fixture)
     {
-        using difference_3over6_2over5 = RationalDifference<Rational<3, 6>, Rational<2, 5>>;
+        using Difference_3o6_2o5 = RationalDifference<Rational<3, 6>, Rational<2, 5>>;
 
-        SYNTROPY_UNIT_EQUAL(difference_3over6_2over5::kNumerator, 3);
-        SYNTROPY_UNIT_EQUAL(difference_3over6_2over5::kDenominator, 30);
+        SYNTROPY_UNIT_EQUAL(Difference_3o6_2o5::kNumerator, 3);
+        SYNTROPY_UNIT_EQUAL(Difference_3o6_2o5::kDenominator, 30);
     })
 
     .TestCase("Subtracting zero from a rational returns a rational equivalent to the latter.", [](auto& fixture)
     {
-        using difference_3over6_0over1 = RationalDifference<Rational<3, 6>, Rational<0, 1>>;
+        using Difference_3o6_0o1 = RationalDifference<Rational<3, 6>, Rational<0, 1>>;
 
-        SYNTROPY_UNIT_EQUAL(difference_3over6_0over1::kNumerator, 3);
-        SYNTROPY_UNIT_EQUAL(difference_3over6_0over1::kDenominator, 6);
+        SYNTROPY_UNIT_EQUAL(Difference_3o6_0o1::kNumerator, 3);
+        SYNTROPY_UNIT_EQUAL(Difference_3o6_0o1::kDenominator, 6);
 
-        using difference_3over6_0over5 = RationalDifference<Rational<3, 6>, Rational<0, 5>>;
+        using Difference_3o6_0o5 = RationalDifference<Rational<3, 6>, Rational<0, 5>>;
 
-        SYNTROPY_UNIT_EQUAL(difference_3over6_0over5::kNumerator, 15);
-        SYNTROPY_UNIT_EQUAL(difference_3over6_0over5::kDenominator, 30);
+        SYNTROPY_UNIT_EQUAL(Difference_3o6_0o5::kNumerator, 15);
+        SYNTROPY_UNIT_EQUAL(Difference_3o6_0o5::kDenominator, 30);
     })
 
     .TestCase("Subtracting a rational from zero returns the negated rational.", [](auto& fixture)
     {
-        using difference_0over1_3over1 = RationalDifference<Rational<0, 1>, Rational<3, 1>>;
+        using Difference_0o1_3o1 = RationalDifference<Rational<0, 1>, Rational<3, 1>>;
 
-        SYNTROPY_UNIT_EQUAL(difference_0over1_3over1::kNumerator, -3);
-        SYNTROPY_UNIT_EQUAL(difference_0over1_3over1::kDenominator, 1);
+        SYNTROPY_UNIT_EQUAL(Difference_0o1_3o1::kNumerator, -3);
+        SYNTROPY_UNIT_EQUAL(Difference_0o1_3o1::kDenominator, 1);
 
-        using difference_0over2_3over5 = RationalDifference<Rational<0, 2>, Rational<3, 5>>;
+        using Difference_0o2_3o5 = RationalDifference<Rational<0, 2>, Rational<3, 5>>;
 
-        SYNTROPY_UNIT_EQUAL(difference_0over2_3over5::kNumerator, -6);
-        SYNTROPY_UNIT_EQUAL(difference_0over2_3over5::kDenominator, 10);
+        SYNTROPY_UNIT_EQUAL(Difference_0o2_3o5::kNumerator, -6);
+        SYNTROPY_UNIT_EQUAL(Difference_0o2_3o5::kDenominator, 10);
     })
 
     .TestCase("Multiplying two rationals returns the unreduced product of both numerators and both denominators.", [](auto& fixture)
     {
-        using product_6over8_9over15 = RationalProduct<Rational<6, 8>, Rational<9, 15>>;
+        using Product_6o8_9o15 = RationalProduct<Rational<6, 8>, Rational<9, 15>>;
 
-        SYNTROPY_UNIT_EQUAL(product_6over8_9over15::kNumerator, 54);
-        SYNTROPY_UNIT_EQUAL(product_6over8_9over15::kDenominator, 120);
+        SYNTROPY_UNIT_EQUAL(Product_6o8_9o15::kNumerator, 54);
+        SYNTROPY_UNIT_EQUAL(Product_6o8_9o15::kDenominator, 120);
 
-        using product_minus6over2_7over11 = RationalProduct<Rational<-6, 2>, Rational<7, 11>>;
+        using Product_Minus6o2_7o11 = RationalProduct<Rational<-6, 2>, Rational<7, 11>>;
 
-        SYNTROPY_UNIT_EQUAL(product_minus6over2_7over11::kNumerator, -42);
-        SYNTROPY_UNIT_EQUAL(product_minus6over2_7over11::kDenominator, 22);
+        SYNTROPY_UNIT_EQUAL(Product_Minus6o2_7o11::kNumerator, -42);
+        SYNTROPY_UNIT_EQUAL(Product_Minus6o2_7o11::kDenominator, 22);
     })
 
     .TestCase("Multiplying a rational by one returns an unreduced ratio equivalent to the former.", [](auto& fixture)
     {
-        using product_6over8_1over1 = RationalProduct<Rational<6, 8>, Rational<1, 1>>;
+        using Product_6o8_1o1 = RationalProduct<Rational<6, 8>, Rational<1, 1>>;
 
-        SYNTROPY_UNIT_EQUAL(product_6over8_1over1::kNumerator, 6);
-        SYNTROPY_UNIT_EQUAL(product_6over8_1over1::kDenominator, 8);
+        SYNTROPY_UNIT_EQUAL(Product_6o8_1o1::kNumerator, 6);
+        SYNTROPY_UNIT_EQUAL(Product_6o8_1o1::kDenominator, 8);
 
-        using product_minus4over4_13over17 = RationalProduct<Rational<-4, 4>, Rational<13, 17>>;
+        using Product_Minus4o4_13o17 = RationalProduct<Rational<-4, 4>, Rational<13, 17>>;
 
-        SYNTROPY_UNIT_EQUAL(product_minus4over4_13over17::kNumerator, -52);
-        SYNTROPY_UNIT_EQUAL(product_minus4over4_13over17::kDenominator, 68);
+        SYNTROPY_UNIT_EQUAL(Product_Minus4o4_13o17::kNumerator, -52);
+        SYNTROPY_UNIT_EQUAL(Product_Minus4o4_13o17::kDenominator, 68);
     })
 
     .TestCase("Dividing a rational by another one returns the unreduced rational equivalent their.", [](auto& fixture)
     {
-        using quotient_6over8_9over15 = RationalQuotient<Rational<6, 8>, Rational<9, 15>>;
+        using Quotient_6o8_9o15 = RationalQuotient<Rational<6, 8>, Rational<9, 15>>;
 
-        SYNTROPY_UNIT_EQUAL(quotient_6over8_9over15::kNumerator, 90);
-        SYNTROPY_UNIT_EQUAL(quotient_6over8_9over15::kDenominator, 72);
+        SYNTROPY_UNIT_EQUAL(Quotient_6o8_9o15::kNumerator, 90);
+        SYNTROPY_UNIT_EQUAL(Quotient_6o8_9o15::kDenominator, 72);
 
-        using quotient_minus6over2_7over11 = RationalQuotient<Rational<-6, 2>, Rational<7, 11>>;
+        using Quotient_Minus6o2_7o11 = RationalQuotient<Rational<-6, 2>, Rational<7, 11>>;
 
-        SYNTROPY_UNIT_EQUAL(quotient_minus6over2_7over11::kNumerator, -66);
-        SYNTROPY_UNIT_EQUAL(quotient_minus6over2_7over11::kDenominator, 14);
+        SYNTROPY_UNIT_EQUAL(Quotient_Minus6o2_7o11::kNumerator, -66);
+        SYNTROPY_UNIT_EQUAL(Quotient_Minus6o2_7o11::kDenominator, 14);
     })
 
     .TestCase("Dividing a rational by one returns an unreduced ratio equivalent to the former.", [](auto& fixture)
     {
-        using quotient_6over8_1over1 = RationalQuotient<Rational<6, 8>, Rational<1, 1>>;
+        using Quotient_6o8_1o1 = RationalQuotient<Rational<6, 8>, Rational<1, 1>>;
 
-        SYNTROPY_UNIT_EQUAL(quotient_6over8_1over1::kNumerator, 6);
-        SYNTROPY_UNIT_EQUAL(quotient_6over8_1over1::kDenominator, 8);
+        SYNTROPY_UNIT_EQUAL(Quotient_6o8_1o1::kNumerator, 6);
+        SYNTROPY_UNIT_EQUAL(Quotient_6o8_1o1::kDenominator, 8);
     })
  
     .TestCase("Dividing one by a rational returns the reciprocal of the latter.", [](auto& fixture)
     {
-        using quotient_1over1_6over7 = RationalQuotient<Rational<1, 1>, Rational<6, 7>>;
+        using Quotient_1o1_6o7 = RationalQuotient<Rational<1, 1>, Rational<6, 7>>;
 
-        SYNTROPY_UNIT_EQUAL(quotient_1over1_6over7::kNumerator, 7);
-        SYNTROPY_UNIT_EQUAL(quotient_1over1_6over7::kDenominator, 6);
+        SYNTROPY_UNIT_EQUAL(Quotient_1o1_6o7::kNumerator, 7);
+        SYNTROPY_UNIT_EQUAL(Quotient_1o1_6o7::kDenominator, 6);
     });
 
 }
