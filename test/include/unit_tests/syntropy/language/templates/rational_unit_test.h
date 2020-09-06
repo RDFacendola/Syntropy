b179fde85674e6a1c98bfc6db1cb1223a4120aed
Raffaele Daniele Facendola <raffaele.facendola@live.com>
2020-09-06 19:14:16 +0200

    Cleaning up rational unit test.

--- test/include/unit_tests/syntropy/language/templates/rational_unit_test.h
+++ test/include/unit_tests/syntropy/language/templates/rational_unit_test.h
@@ -8,145 +8,129 @@
 #include "syntropy/language/templates/rational.h"
 #include "syntropy/unit_test/unit_test.h"
 
-namespace Syntropy::unit_test
+namespace Syntropy::Templates::UnitTest
 {
     /************************************************************************/
     /* RATIONAL TEST FIXTURE                                                */
     /************************************************************************/
 
     /// \brief Rational test fixture.
-    struct RationalTemplatesLanguageSyntropyFixture{};
+    struct RationalTestFixture{};
 
     /************************************************************************/
     /* UNIT TESTS                                                           */
     /************************************************************************/
 
-    inline const auto& rational_templates_language_syntropy_unit_test = MakeAutoUnitTest<RationalTemplatesLanguageSyntropyFixture>("rational.templates.language.syntropy")
+    inline const auto& kRationalTest = MakeAutoUnitTest<RationalTestFixture>("rational.templates.language.syntropy")
 
     .TestCase("Adding two rationals together returns the unreduced sum of the twos.", [](auto& fixture)
     {
-        constexpr auto templates_sum_3over6_2over5_numerator = Templates::RationalSum<Templates::Rational<3, 6>, Templates::Rational<2, 5>>::kNumerator;
-        constexpr auto templates_sum_3over6_2over5_denominator = Templates::RationalSum<Templates::Rational<3, 6>, Templates::Rational<2, 5>>::kDenominator;
+        using sum_3over6_2over5 = RationalSum<Rational<3, 6>, Rational<2, 5>>;
 
-        SYNTROPY_UNIT_EQUAL(templates_sum_3over6_2over5_numerator, 27);
-        SYNTROPY_UNIT_EQUAL(templates_sum_3over6_2over5_denominator, 30);
+        SYNTROPY_UNIT_EQUAL(sum_3over6_2over5::kNumerator, 27);
+        SYNTROPY_UNIT_EQUAL(sum_3over6_2over5::kDenominator, 30);
     })
 
     .TestCase("Adding zero to a rational returns a rational is equivalent to the latter.", [](auto& fixture)
     {
-        auto templates_sum_3over6_0over1_numerator = Templates::RationalSum<Templates::Rational<3, 6>, Templates::Rational<0, 1>>::kNumerator;
-        auto templates_sum_3over6_0over1_denominator = Templates::RationalSum<Templates::Rational<3, 6>, Templates::Rational<0, 1>>::kDenominator;
+        using sum_3over6_0over1 = RationalSum<Rational<3, 6>, Rational<0, 1>>;
 
-        SYNTROPY_UNIT_EQUAL(templates_sum_3over6_0over1_numerator, 3);
-        SYNTROPY_UNIT_EQUAL(templates_sum_3over6_0over1_denominator, 6);
+        SYNTROPY_UNIT_EQUAL(sum_3over6_0over1::kNumerator, 3);
+        SYNTROPY_UNIT_EQUAL(sum_3over6_0over1::kDenominator, 6);
 
-        auto templates_sum_0over6_3over1_numerator = Templates::RationalSum<Templates::Rational<0, 6>, Templates::Rational<3, 1>>::kNumerator;
-        auto templates_sum_0over6_3over1_denominator = Templates::RationalSum<Templates::Rational<0, 6>, Templates::Rational<3, 1>>::kDenominator;
+        using sum_0over6_3over1 = RationalSum<Rational<0, 6>, Rational<3, 1>>;
 
-        SYNTROPY_UNIT_EQUAL(templates_sum_0over6_3over1_numerator, 18);
-        SYNTROPY_UNIT_EQUAL(templates_sum_0over6_3over1_denominator, 6);
+        SYNTROPY_UNIT_EQUAL(sum_0over6_3over1::kNumerator, 18);
+        SYNTROPY_UNIT_EQUAL(sum_0over6_3over1::kDenominator, 6);
     })
 
     .TestCase("Subtracting a rational from another rational returns the unreduced difference of the twos.", [](auto& fixture)
     {
-        auto templates_difference_3over6_2over5_numerator = Templates::RationalDifference<Templates::Rational<3, 6>, Templates::Rational<2, 5>>::kNumerator;
-        auto templates_difference_3over6_2over5_denominator = Templates::RationalDifference<Templates::Rational<3, 6>, Templates::Rational<2, 5>>::kDenominator;
+        using difference_3over6_2over5 = RationalDifference<Rational<3, 6>, Rational<2, 5>>;
 
-        SYNTROPY_UNIT_EQUAL(templates_difference_3over6_2over5_numerator, 3);
-        SYNTROPY_UNIT_EQUAL(templates_difference_3over6_2over5_denominator, 30);
+        SYNTROPY_UNIT_EQUAL(difference_3over6_2over5::kNumerator, 3);
+        SYNTROPY_UNIT_EQUAL(difference_3over6_2over5::kDenominator, 30);
     })
 
     .TestCase("Subtracting zero from a rational returns a rational equivalent to the latter.", [](auto& fixture)
     {
-        auto templates_difference_3over6_0over1_numerator = Templates::RationalDifference<Templates::Rational<3, 6>, Templates::Rational<0, 1>>::kNumerator;
-        auto templates_difference_3over6_0over1_denominator = Templates::RationalDifference<Templates::Rational<3, 6>, Templates::Rational<0, 1>>::kDenominator;
+        using difference_3over6_0over1 = RationalDifference<Rational<3, 6>, Rational<0, 1>>;
 
-        SYNTROPY_UNIT_EQUAL(templates_difference_3over6_0over1_numerator, 3);
-        SYNTROPY_UNIT_EQUAL(templates_difference_3over6_0over1_denominator, 6);
+        SYNTROPY_UNIT_EQUAL(difference_3over6_0over1::kNumerator, 3);
+        SYNTROPY_UNIT_EQUAL(difference_3over6_0over1::kDenominator, 6);
 
-        auto templates_difference_3over6_0over5_numerator = Templates::RationalDifference<Templates::Rational<3, 6>, Templates::Rational<0, 5>>::kNumerator;
-        auto templates_difference_3over6_0over5_denominator = Templates::RationalDifference<Templates::Rational<3, 6>, Templates::Rational<0, 5>>::kDenominator;
+        using difference_3over6_0over5 = RationalDifference<Rational<3, 6>, Rational<0, 5>>;
 
-        SYNTROPY_UNIT_EQUAL(templates_difference_3over6_0over5_numerator, 15);
-        SYNTROPY_UNIT_EQUAL(templates_difference_3over6_0over5_denominator, 30);
+        SYNTROPY_UNIT_EQUAL(difference_3over6_0over5::kNumerator, 15);
+        SYNTROPY_UNIT_EQUAL(difference_3over6_0over5::kDenominator, 30);
     })
 
     .TestCase("Subtracting a rational from zero returns the negated rational.", [](auto& fixture)
     {
-        auto templates_difference_0over1_3over1_numerator = Templates::RationalDifference<Templates::Rational<0, 1>, Templates::Rational<3, 1>>::kNumerator;
-        auto templates_difference_0over1_3over1_denominator = Templates::RationalDifference<Templates::Rational<0, 1>, Templates::Rational<3, 1>>::kDenominator;
+        using difference_0over1_3over1 = RationalDifference<Rational<0, 1>, Rational<3, 1>>;
 
-        SYNTROPY_UNIT_EQUAL(templates_difference_0over1_3over1_numerator, -3);
-        SYNTROPY_UNIT_EQUAL(templates_difference_0over1_3over1_denominator, 1);
+        SYNTROPY_UNIT_EQUAL(difference_0over1_3over1::kNumerator, -3);
+        SYNTROPY_UNIT_EQUAL(difference_0over1_3over1::kDenominator, 1);
 
-        auto templates_difference_0over2_3over5_numerator = Templates::RationalDifference<Templates::Rational<0, 2>, Templates::Rational<3, 5>>::kNumerator;
-        auto templates_difference_0over2_3over5_denominator = Templates::RationalDifference<Templates::Rational<0, 2>, Templates::Rational<3, 5>>::kDenominator;
+        using difference_0over2_3over5 = RationalDifference<Rational<0, 2>, Rational<3, 5>>;
 
-        SYNTROPY_UNIT_EQUAL(templates_difference_0over2_3over5_numerator, -6);
-        SYNTROPY_UNIT_EQUAL(templates_difference_0over2_3over5_denominator, 10);
+        SYNTROPY_UNIT_EQUAL(difference_0over2_3over5::kNumerator, -6);
+        SYNTROPY_UNIT_EQUAL(difference_0over2_3over5::kDenominator, 10);
     })
 
     .TestCase("Multiplying two rationals returns the unreduced product of both numerators and both denominators.", [](auto& fixture)
     {
-        auto templates_product_6over8_9over15_numerator = Templates::RationalProduct<Templates::Rational<6, 8>, Templates::Rational<9, 15>>::kNumerator;
-        auto templates_product_6over8_9over15_denominator = Templates::RationalProduct<Templates::Rational<6, 8>, Templates::Rational<9, 15>>::kDenominator;
+        using product_6over8_9over15 = RationalProduct<Rational<6, 8>, Rational<9, 15>>;
 
-        SYNTROPY_UNIT_EQUAL(templates_product_6over8_9over15_numerator, 54);
-        SYNTROPY_UNIT_EQUAL(templates_product_6over8_9over15_denominator, 120);
+        SYNTROPY_UNIT_EQUAL(product_6over8_9over15::kNumerator, 54);
+        SYNTROPY_UNIT_EQUAL(product_6over8_9over15::kDenominator, 120);
 
-        auto templates_product_minus6over2_7over11_numerator = Templates::RationalProduct<Templates::Rational<-6, 2>, Templates::Rational<7, 11>>::kNumerator;
-        auto templates_product_minus6over2_7over11_denominator = Templates::RationalProduct<Templates::Rational<-6, 2>, Templates::Rational<7, 11>>::kDenominator;
+        using product_minus6over2_7over11 = RationalProduct<Rational<-6, 2>, Rational<7, 11>>;
 
-        SYNTROPY_UNIT_EQUAL(templates_product_minus6over2_7over11_numerator, -42);
-        SYNTROPY_UNIT_EQUAL(templates_product_minus6over2_7over11_denominator, 22);
+        SYNTROPY_UNIT_EQUAL(product_minus6over2_7over11::kNumerator, -42);
+        SYNTROPY_UNIT_EQUAL(product_minus6over2_7over11::kDenominator, 22);
     })
 
     .TestCase("Multiplying a rational by one returns an unreduced ratio equivalent to the former.", [](auto& fixture)
     {
-        auto templates_product_6over8_1over1_numerator = Templates::RationalProduct<Templates::Rational<6, 8>, Templates::Rational<1, 1>>::kNumerator;
-        auto templates_product_6over8_1over1_denominator = Templates::RationalProduct<Templates::Rational<6, 8>, Templates::Rational<1, 1>>::kDenominator;
+        using product_6over8_1over1 = RationalProduct<Rational<6, 8>, Rational<1, 1>>;
 
-        SYNTROPY_UNIT_EQUAL(templates_product_6over8_1over1_numerator, 6);
-        SYNTROPY_UNIT_EQUAL(templates_product_6over8_1over1_denominator, 8);
+        SYNTROPY_UNIT_EQUAL(product_6over8_1over1::kNumerator, 6);
+        SYNTROPY_UNIT_EQUAL(product_6over8_1over1::kDenominator, 8);
 
-        auto templates_product_minus4over4_13over17_numerator = Templates::RationalProduct<Templates::Rational<-4, 4>, Templates::Rational<13, 17>>::kNumerator;
-        auto templates_product_minus4over4_13over17_denominator = Templates::RationalProduct<Templates::Rational<-4, 4>, Templates::Rational<13, 17>>::kDenominator;
+        using product_minus4over4_13over17 = RationalProduct<Rational<-4, 4>, Rational<13, 17>>;
 
-        SYNTROPY_UNIT_EQUAL(templates_product_minus4over4_13over17_numerator, -52);
-        SYNTROPY_UNIT_EQUAL(templates_product_minus4over4_13over17_denominator, 68);
+        SYNTROPY_UNIT_EQUAL(product_minus4over4_13over17::kNumerator, -52);
+        SYNTROPY_UNIT_EQUAL(product_minus4over4_13over17::kDenominator, 68);
     })
 
     .TestCase("Dividing a rational by another one returns the unreduced rational equivalent their.", [](auto& fixture)
     {
-        auto templates_quotient_6over8_9over15_numerator = Templates::RationalQuotient<Templates::Rational<6, 8>, Templates::Rational<9, 15>>::kNumerator;
-        auto templates_quotient_6over8_9over15_denominator = Templates::RationalQuotient<Templates::Rational<6, 8>, Templates::Rational<9, 15>>::kDenominator;
+        using quotient_6over8_9over15 = RationalQuotient<Rational<6, 8>, Rational<9, 15>>;
 
-        SYNTROPY_UNIT_EQUAL(templates_quotient_6over8_9over15_numerator, 90);
-        SYNTROPY_UNIT_EQUAL(templates_quotient_6over8_9over15_denominator, 72);
+        SYNTROPY_UNIT_EQUAL(quotient_6over8_9over15::kNumerator, 90);
+        SYNTROPY_UNIT_EQUAL(quotient_6over8_9over15::kDenominator, 72);
 
-        auto templates_quotient_minus6over2_7over11_numerator = Templates::RationalQuotient<Templates::Rational<-6, 2>, Templates::Rational<7, 11>>::kNumerator;
-        auto templates_quotient_minus6over2_7over11_denominator = Templates::RationalQuotient<Templates::Rational<-6, 2>, Templates::Rational<7, 11>>::kDenominator;
+        using quotient_minus6over2_7over11 = RationalQuotient<Rational<-6, 2>, Rational<7, 11>>;
 
-        SYNTROPY_UNIT_EQUAL(templates_quotient_minus6over2_7over11_numerator, -66);
-        SYNTROPY_UNIT_EQUAL(templates_quotient_minus6over2_7over11_denominator, 14);
+        SYNTROPY_UNIT_EQUAL(quotient_minus6over2_7over11::kNumerator, -66);
+        SYNTROPY_UNIT_EQUAL(quotient_minus6over2_7over11::kDenominator, 14);
     })
 
     .TestCase("Dividing a rational by one returns an unreduced ratio equivalent to the former.", [](auto& fixture)
     {
-        auto templates_quotient_6over8_1over1_numerator = Templates::RationalQuotient<Templates::Rational<6, 8>, Templates::Rational<1, 1>>::kNumerator;
-        auto templates_quotient_6over8_1over1_denominator = Templates::RationalQuotient<Templates::Rational<6, 8>, Templates::Rational<1, 1>>::kDenominator;
+        using quotient_6over8_1over1 = RationalQuotient<Rational<6, 8>, Rational<1, 1>>;
 
-        SYNTROPY_UNIT_EQUAL(templates_quotient_6over8_1over1_numerator, 6);
-        SYNTROPY_UNIT_EQUAL(templates_quotient_6over8_1over1_denominator, 8);
+        SYNTROPY_UNIT_EQUAL(quotient_6over8_1over1::kNumerator, 6);
+        SYNTROPY_UNIT_EQUAL(quotient_6over8_1over1::kDenominator, 8);
     })
  
     .TestCase("Dividing one by a rational returns the reciprocal of the latter.", [](auto& fixture)
     {
-        auto templates_quotient_1over1_6over7_numerator = Templates::RationalQuotient<Templates::Rational<1, 1>, Templates::Rational<6, 7>>::kNumerator;
-        auto templates_quotient_1over1_6over7_denominator = Templates::RationalQuotient<Templates::Rational<1, 1>, Templates::Rational<6, 7>>::kDenominator;
+        using quotient_1over1_6over7 = RationalQuotient<Rational<1, 1>, Rational<6, 7>>;
 
-        SYNTROPY_UNIT_EQUAL(templates_quotient_1over1_6over7_numerator, 7);
-        SYNTROPY_UNIT_EQUAL(templates_quotient_1over1_6over7_denominator, 6);
+        SYNTROPY_UNIT_EQUAL(quotient_1over1_6over7::kNumerator, 7);
+        SYNTROPY_UNIT_EQUAL(quotient_1over1_6over7::kDenominator, 6);
     });
 
 }
