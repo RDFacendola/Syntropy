063b3693ecef6c1cadd0956bf71f8bb1e05f39c4
Raffaele Daniele Facendola <raffaele.facendola@live.com>
2020-10-04 17:56:59 +0200

    Moving tuple_details file under "experimental".

---
+++ Syntropy/include/syntropy/experimental/core/foundation/tuple.h
@@ -0,0 +1,18 @@
+/// \file tuple.h
+/// \brief This header is part of Syntropy core module. It contains definitions for tuples.
+///
+/// \author Raffaele D. Facendola - 2020.
+
+#pragma once
+
+#include "syntropy/language/foundation.h"
+
+namespace Syntropy
+{
+    /************************************************************************/
+    /* TUPLE <ELEMENTS...>                                                  */
+    /************************************************************************/
+
+
+
+}
