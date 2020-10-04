e2f0c73245dbc744b49a8bc4671157a3096daf2d
Raffaele Daniele Facendola <raffaele.facendola@live.com>
2020-10-04 18:38:14 +0200

    Moving byte unit test under Syntropy::Memory::UnitTest.

--- test/include/unit_tests/syntropy/memory/bytes_unit_test.h
+++ test/include/unit_tests/syntropy/memory/bytes_unit_test.h
@@ -11,7 +11,7 @@
 #include "syntropy/memory/byte.h"
 #include "syntropy/unit_test/unit_test.h"
 
-namespace Syntropy::unit_test
+namespace Syntropy::Memory::UnitTest
 {
     /************************************************************************/
     /* BYTES TEST FIXTURE                                                   */
@@ -21,32 +21,32 @@ namespace Syntropy::unit_test
     struct BytesTestFixture
     {
         /// \brief Memory buffer.
-        Memory::Byte buffer_[10];
+        Byte buffer_[10];
 
         /// \brief Read-write pointer to a buffer.
-        Pointer<Memory::Byte> read_write_ptr_;
+        Pointer<Byte> read_write_ptr_;
 
         /// \brief Read-only pointer to a buffer.
-        Pointer<const Memory::Byte> read_only_ptr_;
+        Pointer<const Byte> read_only_ptr_;
 
         /// \brief Default constructed bytes object.
-        const Memory::Bytes bytes_default_ = Memory::Bytes{};
+        const Bytes bytes_default_ = Bytes{};
 
         /// \brief Numeric constants.
 
-        const Memory::Bytes bytes0_ = Memory::Bytes{ 0 };
-        const Memory::Bytes bytes1_ = Memory::Bytes{ 1 };
-        const Memory::Bytes bytes2_ = Memory::Bytes{ 2 };
-        const Memory::Bytes bytes3_ = Memory::Bytes{ 3 };
-        const Memory::Bytes bytes4_ = Memory::Bytes{ 4 };
-        const Memory::Bytes bytes5_ = Memory::Bytes{ 5 };
-        const Memory::Bytes bytes6_ = Memory::Bytes{ 6 };
-        const Memory::Bytes bytes7_ = Memory::Bytes{ 7 };
-        const Memory::Bytes bytes8_ = Memory::Bytes{ 8 };
-        const Memory::Bytes bytes10_ = Memory::Bytes{ 10 };
+        const Bytes bytes0_ = Bytes{ 0 };
+        const Bytes bytes1_ = Bytes{ 1 };
+        const Bytes bytes2_ = Bytes{ 2 };
+        const Bytes bytes3_ = Bytes{ 3 };
+        const Bytes bytes4_ = Bytes{ 4 };
+        const Bytes bytes5_ = Bytes{ 5 };
+        const Bytes bytes6_ = Bytes{ 6 };
+        const Bytes bytes7_ = Bytes{ 7 };
+        const Bytes bytes8_ = Bytes{ 8 };
+        const Bytes bytes10_ = Bytes{ 10 };
 
         /// \brief Modifiable bytes object.
-        Memory::Bytes bytes_lhs_ = Memory::Bytes{ 5 };
+        Bytes bytes_lhs_ = Bytes{ 5 };
 
         void Before();
     };
@@ -57,21 +57,21 @@ namespace Syntropy::unit_test
 
     inline const auto& bytes_unit_test = MakeAutoUnitTest<BytesTestFixture>("bytes.memory.syntropy")
 
-    .TestCase("Memory::Bytes are convertible to and from integers.", [](auto& fixture)
+    .TestCase("Bytes are convertible to and from integers.", [](auto& fixture)
     {
-        SYNTROPY_UNIT_EQUAL(Memory::ToInt(fixture.bytes2_), 2);
-        SYNTROPY_UNIT_EQUAL(Memory::ToBytes(2), fixture.bytes2_);
+        SYNTROPY_UNIT_EQUAL(ToInt(fixture.bytes2_), 2);
+        SYNTROPY_UNIT_EQUAL(ToBytes(2), fixture.bytes2_);
     })
 
     .TestCase("SizeOf return a bytes amount equal to the size of a type, converted to a bytes amount.", [](auto& fixture)
     {
-        using namespace Memory::Literals;
+        using namespace Literals;
 
-        SYNTROPY_UNIT_EQUAL(Memory::SizeOf<Fix32>(), 4_Bytes);
-        SYNTROPY_UNIT_EQUAL(Memory::SizeOf(Fix64{}), 8_Bytes);
+        SYNTROPY_UNIT_EQUAL(SizeOf<Fix32>(), 4_Bytes);
+        SYNTROPY_UNIT_EQUAL(SizeOf(Fix64{}), 8_Bytes);
     })
 
-    .TestCase("Default initialized Memory::Bytes are empty.", [](auto& fixture)
+    .TestCase("Default initialized Bytes are empty.", [](auto& fixture)
     {
         SYNTROPY_UNIT_EQUAL(fixture.bytes_default_, fixture.bytes0_);
     })
@@ -114,40 +114,40 @@ namespace Syntropy::unit_test
 
     .TestCase("Division remainder of two byte amounts is equal to an integer equal to the quotient remainder of the two integer values.", [](auto& fixture)
     {
-        SYNTROPY_UNIT_EQUAL(fixture.bytes7_% fixture.bytes4_, Memory::Bytes{ 3 });
+        SYNTROPY_UNIT_EQUAL(fixture.bytes7_% fixture.bytes4_, Bytes{ 3 });
     })
 
     .TestCase("Pre-incrementing a byte amount produces a byte amount which is equal to the integer value pre-incremented by one.", [](auto& fixture)
     {
-        SYNTROPY_UNIT_EQUAL(++fixture.bytes_lhs_, Memory::Bytes{ 6 });
+        SYNTROPY_UNIT_EQUAL(++fixture.bytes_lhs_, Bytes{ 6 });
     })
 
     .TestCase("Pre-decrementing a byte amount produces a byte amount which is equal to the integer value pre-decremented by one.", [](auto& fixture)
     {
-        SYNTROPY_UNIT_EQUAL(--fixture.bytes_lhs_, Memory::Bytes{ 4 });
+        SYNTROPY_UNIT_EQUAL(--fixture.bytes_lhs_, Bytes{ 4 });
     })
 
     .TestCase("Post-incrementing a byte amount produces a byte amount which is equal to the integer value post-incremented by one.", [](auto& fixture)
     {
-        SYNTROPY_UNIT_EQUAL(fixture.bytes_lhs_++, Memory::Bytes{ 5 });
-        SYNTROPY_UNIT_EQUAL(fixture.bytes_lhs_, Memory::Bytes{ 6 });
+        SYNTROPY_UNIT_EQUAL(fixture.bytes_lhs_++, Bytes{ 5 });
+        SYNTROPY_UNIT_EQUAL(fixture.bytes_lhs_, Bytes{ 6 });
     })
 
     .TestCase("Post-decrementing a byte amount produces a byte amount which is equal to the integer value post-decremented by one.", [](auto& fixture)
     {
-        SYNTROPY_UNIT_EQUAL(fixture.bytes_lhs_--, Memory::Bytes{ 5 });
-        SYNTROPY_UNIT_EQUAL(fixture.bytes_lhs_, Memory::Bytes{ 4 });
+        SYNTROPY_UNIT_EQUAL(fixture.bytes_lhs_--, Bytes{ 5 });
+        SYNTROPY_UNIT_EQUAL(fixture.bytes_lhs_, Bytes{ 4 });
     })
 
-    .TestCase("Memory::Bytes literals behaves as binary metric prefixes equivalent.", [](auto& fixture)
+    .TestCase("Bytes literals behaves as binary metric prefixes equivalent.", [](auto& fixture)
     {
-        using namespace Memory::Literals;
+        using namespace Literals;
 
-        SYNTROPY_UNIT_EQUAL(1_Bytes,   Memory::Bytes{ 0x0000000000000001ll });
-        SYNTROPY_UNIT_EQUAL(2_KiBytes, Memory::Bytes{ 0x0000000000000800ll });
-        SYNTROPY_UNIT_EQUAL(3_MiBytes, Memory::Bytes{ 0x0000000000300000ll });
-        SYNTROPY_UNIT_EQUAL(4_GiBytes, Memory::Bytes{ 0x0000000100000000ll });
-        SYNTROPY_UNIT_EQUAL(5_TiBytes, Memory::Bytes{ 0x0000050000000000ll });
+        SYNTROPY_UNIT_EQUAL(1_Bytes,   Bytes{ 0x0000000000000001ll });
+        SYNTROPY_UNIT_EQUAL(2_KiBytes, Bytes{ 0x0000000000000800ll });
+        SYNTROPY_UNIT_EQUAL(3_MiBytes, Bytes{ 0x0000000000300000ll });
+        SYNTROPY_UNIT_EQUAL(4_GiBytes, Bytes{ 0x0000000100000000ll });
+        SYNTROPY_UNIT_EQUAL(5_TiBytes, Bytes{ 0x0000050000000000ll });
     });
 
     /************************************************************************/
@@ -156,7 +156,7 @@ namespace Syntropy::unit_test
 
     inline void BytesTestFixture::Before()
     {
-        bytes_lhs_ = Memory::Bytes{ 5 };
+        bytes_lhs_ = Bytes{ 5 };
 
         read_write_ptr_ = &buffer_[4];
         read_only_ptr_ = &buffer_[4];
