41fa360cf3d0045e5a62ef5207e460147aa6f3a3
Raffaele Daniele Facendola <raffaele.facendola@live.com>
2020-11-01 19:46:35 +0100

    Moving ApplyAt and ApplyLockstep under templates/functional.h

---
+++ Syntropy/include/syntropy/language/templates/details/functional_details.h
@@ -0,0 +1,42 @@
+
+/// \file tuple_details.h
+/// \brief This header is part of Syntropy language module. It contains implementation details for functional-style templates.
+///
+/// \author Raffaele D. Facendola - 2020.
+
+#pragma once
+
+#include "syntropy/language/templates/type_list.h"
+
+// ===========================================================================
+
+namespace Syntropy::Templates
+{
+    /************************************************************************/
+    /* FORWARD DECLARATIONS                                                 */
+    /************************************************************************/
+
+    template <Int VIndex, typename TFunction, typename... TTuples>
+    constexpr decltype(auto) ApplyAt(TFunction&& function, TTuples&&... tuples) noexcept;
+
+}
+
+// ===========================================================================
+
+namespace Syntropy::Templates::Details
+{
+    /************************************************************************/
+    /* LOCKSTEP                                                             */
+    /************************************************************************/
+
+    // ApplyLockstep.
+    // ==============
+
+    template <typename TFunction, typename... TTuples, Int... VIndexes>
+    constexpr void ApplyLockstep(IntegerSequence<VIndexes...>, TFunction&& function, TTuples&&... tuples) noexcept
+    {
+        (ApplyAt<VIndexes>(Forward<TFunction>(function), Forward<TTuples>(tuples)...), ...);
+    }
+}
+
+// ===========================================================================
