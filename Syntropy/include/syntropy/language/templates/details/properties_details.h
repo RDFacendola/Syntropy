a43761028e3bd7ec9dde7e87a9df94e5f1e8c2c1
Raffaele Daniele Facendola <raffaele.facendola@live.com>
2020-11-02 01:14:24 +0100

    Moving detail implementation of properties.h under properties_details.h. Properties-related traits now support TypeLists.

--- Syntropy/include/syntropy/language/templates/details/properties_details.h
+++ Syntropy/include/syntropy/language/templates/details/properties_details.h
@@ -1,6 +1,6 @@
 
-/// \file property_details.h
-/// \brief This header is part of Syntropy language module. It contains implementation details for functional-style templates.
+/// \file properties_details.h
+/// \brief This header is part of Syntropy language module. It contains implementation details about properties-related template machinery.
 ///
 /// \author Raffaele D. Facendola - 2020.
 
@@ -8,3 +8,102 @@
 
 #include <type_traits>
 
+#include "syntropy/language/foundation/types.h"
+
+#include "syntropy/language/templates/details/type_list_details.h"
+
+// ===========================================================================
+
+namespace Syntropy::Templates::Details
+{
+    /************************************************************************/
+    /* IS CONST                                                             */
+    /************************************************************************/
+
+    /// \brief Constant equal to true if TType is constant, equal to false otherwise.
+    template <typename TType>
+    inline constexpr Bool IsConst = std::is_const_v<TType>;
+
+    /// \brief Specialization for type lists.
+    template <typename... TTypes>
+    inline constexpr Bool IsConst<TypeList<TTypes...>> = (IsConst<TTypes> && ...);
+
+    /// \brief Constant equal to true if TType is constant (read-only), equal to false otherwise.
+    template <typename TType>
+    inline constexpr Bool IsReadOnly = std::is_const_v<TType>;
+
+    /// \brief Specialization for type lists.
+    template <typename... TTypes>
+    inline constexpr Bool IsReadOnly<TypeList<TTypes...>> = (IsReadOnly<TTypes> && ...);
+
+    /// \brief Constant equal to true if TType is non-constant (read-write), equal to false otherwise.
+    template <typename TType>
+    inline constexpr Bool IsReadWrite = !std::is_const_v<TType>;
+
+    /// \brief Specialization for type lists.
+    template <typename... TTypes>
+    inline constexpr Bool IsReadWrite<TypeList<TTypes...>> = (IsReadWrite<TTypes> && ...);
+
+    /************************************************************************/
+    /* IS TRIVIAL                                                           */
+    /************************************************************************/
+
+    /// \brief Constant equal to true if TType is trivial, equal to false otherwise.
+    template <typename TType>
+    inline constexpr Bool IsTrivial = std::is_trivial_v<TType>;
+
+    /// \brief Specialization for type lists.
+    template <typename... TTypes>
+    inline constexpr Bool IsTrivial<TypeList<TTypes...>> = (IsTrivial<TTypes> && ...);
+
+    /************************************************************************/
+    /* IS TRIVIALLY COPYABLE                                                */
+    /************************************************************************/
+
+    /// \brief Constant equal to true if TType is trivially copyable, equal to false otherwise.
+    template <typename TType>
+    inline constexpr Bool IsTriviallyCopyable = std::is_trivially_copyable_v<TType>;
+
+    /// \brief Specialization for type lists.
+    template <typename... TTypes>
+    inline constexpr Bool IsTriviallyCopyable<TypeList<TTypes...>> = (IsTriviallyCopyable<TTypes> && ...);
+
+    /************************************************************************/
+    /* IS STANDARD LAYOUT                                                   */
+    /************************************************************************/
+
+    /// \brief Constant equal to true if TType is a standard-layout class, equal to false otherwise.
+    template <typename TType>
+    inline constexpr Bool IsStandardLayout = std::is_standard_layout_v<TType>;
+
+    /// \brief Specialization for type lists.
+    template <typename... TTypes>
+    inline constexpr Bool IsStandardLayout<TypeList<TTypes...>> = (IsStandardLayout<TTypes> && ...);
+
+    /************************************************************************/
+    /* IS POLYMORPHIC                                                       */
+    /************************************************************************/
+
+    /// \brief Constant equal to true if TType is polymorphic class type, equal to false otherwise.
+    template <typename TType>
+    inline constexpr Bool IsPolymorphic = std::is_polymorphic_v<TType>;
+
+    /// \brief Specialization for type lists.
+    template <typename... TTypes>
+    inline constexpr Bool IsPolymorphic<TypeList<TTypes...>> = (IsPolymorphic<TTypes> && ...);
+
+    /************************************************************************/
+    /* IS FINAL                                                             */
+    /************************************************************************/
+
+    /// \brief Constant equal to true if TType is final class type, equal to false otherwise.
+    template <typename TType>
+    inline constexpr Bool IsFinal = std::is_final_v<TType>;
+
+    /// \brief Specialization for type lists.
+    template <typename... TTypes>
+    inline constexpr Bool IsFinal<TypeList<TTypes...>> = (IsFinal<TTypes> && ...);
+
+}
+
+// ===========================================================================
--- Syntropy/include/syntropy/language/templates/properties.h
+++ Syntropy/include/syntropy/language/templates/properties.h
@@ -6,10 +6,11 @@
 
 #pragma once
 
-#include <type_traits>
-
 #include "syntropy/language/foundation/types.h"
+
 #include "syntropy/language/templates/details/properties_details.h"
+
+// ===========================================================================
 
 namespace Syntropy::Templates
 {
@@ -19,15 +20,15 @@ namespace Syntropy::Templates
 
     /// \brief Constant equal to true if TType is constant, equal to false otherwise.
     template <typename TType>
-    inline constexpr Bool IsConst = std::is_const_v<TType>;
+    inline constexpr Bool IsConst = Details::IsConst<TType>;
 
     /// \brief Constant equal to true if TType is constant (read-only), equal to false otherwise.
     template <typename TType>
-    inline constexpr Bool IsReadOnly = IsConst<TType>;
+    inline constexpr Bool IsReadOnly = Details::IsReadOnly<TType>;
 
     /// \brief Constant equal to true if TType is non-constant (read-write), equal to false otherwise.
     template <typename TType>
-    inline constexpr Bool IsReadWrite = !IsConst<TType>;
+    inline constexpr Bool IsReadWrite = Details::IsReadWrite<TType>;
 
     /************************************************************************/
     /* IS TRIVIAL                                                           */
@@ -35,7 +36,7 @@ namespace Syntropy::Templates
 
     /// \brief Constant equal to true if TType is trivial, equal to false otherwise.
     template <typename TType>
-    inline constexpr Bool IsTrivial = std::is_trivial_v<TType>;
+    inline constexpr Bool IsTrivial = Details::IsTrivial<TType>;
 
     /************************************************************************/
     /* IS TRIVIALLY COPYABLE                                                */
@@ -43,7 +44,7 @@ namespace Syntropy::Templates
 
     /// \brief Constant equal to true if TType is trivially copyable, equal to false otherwise.
     template <typename TType>
-    inline constexpr Bool IsTriviallyCopyable = std::is_trivially_copyable_v<TType>;
+    inline constexpr Bool IsTriviallyCopyable = Details::IsTriviallyCopyable<TType>;
 
     /************************************************************************/
     /* IS STANDARD LAYOUT                                                   */
@@ -51,7 +52,7 @@ namespace Syntropy::Templates
 
     /// \brief Constant equal to true if TType is a standard-layout class, equal to false otherwise.
     template <typename TType>
-    inline constexpr Bool IsStandardLayout = std::is_standard_layout_v<TType>;
+    inline constexpr Bool IsStandardLayout = Details::IsStandardLayout<TType>;
 
     /************************************************************************/
     /* IS POLYMORPHIC                                                       */
@@ -59,7 +60,7 @@ namespace Syntropy::Templates
 
     /// \brief Constant equal to true if TType is polymorphic class type, equal to false otherwise.
     template <typename TType>
-    inline constexpr Bool IsPolymorphic = std::is_polymorphic_v<TType>;
+    inline constexpr Bool IsPolymorphic = Details::IsPolymorphic<TType>;
 
     /************************************************************************/
     /* IS FINAL                                                             */
@@ -67,5 +68,7 @@ namespace Syntropy::Templates
 
     /// \brief Constant equal to true if TType is final class type, equal to false otherwise.
     template <typename TType>
-    inline constexpr Bool IsFinal = std::is_final_v<TType>;
+    inline constexpr Bool IsFinal = Details::IsFinal<TType>;
 }
+
+// ===========================================================================
