ebe5db2c0881d056464f61a22da24ddef749900e
Raffaele Daniele Facendola <raffaele.facendola@live.com>
2021-01-17 23:58:26 +0100

    Restoring ReverseRange.

---
+++ Syntropy/include/syntropy/core/concepts/reverse_range.h
@@ -0,0 +1,48 @@
+
+/// \file reverse_range.h
+/// \brief This header is part of the Syntropy core module. It contains definitions for adapters used to iterate a range in reverse order.
+///
+/// Ranges specifications based on the awesome https://www.slideshare.net/rawwell/iteratorsmustgo
+/// 
+/// \author Raffaele D. Facendola - Nov 2020
+
+#pragma once
+
+#include "syntropy/language/foundation/foundation.h"
+
+#include "syntropy/core/concepts/bidirectional_range.h"
+
+#include "syntropy/core/concepts/details/reverse_range_details.h"
+
+// ===========================================================================
+
+namespace Syntropy::Ranges
+{
+    /************************************************************************/
+    /* NON-MEMBER FUNCTIONS                                                 */
+    /************************************************************************/
+
+    /// \brief Obtain a reversed range.
+    template <Ranges::Concepts::BidirectionalRange TRange>
+    auto Reverse(Immutable<TRange> range) noexcept;
+}
+
+// ===========================================================================
+
+namespace Syntropy::Ranges
+{
+    /************************************************************************/
+    /* IMPLEMENTATION                                                       */
+    /************************************************************************/
+
+    // Non-member functions.
+    // =====================
+
+    template <Ranges::Concepts::BidirectionalRange TRange>
+    auto Reverse(Immutable<TRange> range) noexcept
+    {
+        return Details::Reverse(range);
+    }
+}
+
+// ===========================================================================
