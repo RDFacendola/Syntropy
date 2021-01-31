39fd5a5e2daf1c842c5077f9f67c1da0f5aa4560
Raffaele Daniele Facendola <raffaele.facendola@live.com>
2021-01-31 22:28:53 +0100

    Adding empty project with basic configuration.

---
+++ empty/src/empty/main.cpp
@@ -0,0 +1,20 @@
+/// \file main.cpp
+/// A simple hello world project.
+///
+/// \author Raffaele D. Facendola - January 2021
+
+// =========================================================================  //
+
+#include <iostream>
+
+int main()
+{
+   if constexpr(true)
+   {
+      std::cout << "Hello, Syntropy\n";
+   }
+
+   return 0;
+}
+
+// =========================================================================  //
