63ec2c4470874e8884e6816755b72a15cbcbff79
Raffaele Daniele Facendola <raffaele.facendola@live.com>
2021-02-01 13:57:22 +0100

    Updating syntropy/language/preprocessor.

--- syntropy/include/syntropy/language/preprocessor/macro.h
+++ syntropy/include/syntropy/language/preprocessor/macro.h
@@ -1,6 +1,7 @@
 
 /// \file macro.h
-/// \brief This header is part of Syntropy language module. It contains macro definitions.
+/// \brief This header is part of Syntropy language module.
+///         It contains macro definitions.
 ///
 /// \author Raffaele D. Facendola - Nov 2016
 
@@ -17,14 +18,21 @@ namespace Syntropy
     /* MACROS                                                               */
     /************************************************************************/
 
-    /// \brief Special macro used to "forward-declare" a macro and move its definition somewhere else.
-    /// This macro is intended to separate macro declaration site from definition and to avoid clutter near the macro declaration site.
+    /// \brief Special macro used to "forward-declare" a macro and move
+    ///         its definition somewhere else.
+    /// This macro is intended to separate macro declaration site
+    ///  from definition and to avoid clutter near the macro declaration site.
     /// Declaration and definition site must exist in the same file!
-    /// \usage 
-    /// Declaration site. Forward declaration only. Any usage before definition will trigger a static assert.
+    /// \usage
+    ///
+    /// Declaration site. Forward declaration only.
+    ///  Any usage before definition will trigger a static assert.
+    ///
     ///  #define MY_MACRO(arg0, arg1) SYNTROPY_MACRO_DECLARATION(arg0, arg1)
-    /// 
-    /// Definition site. Undef previous forward declaration and provide actual definition.
+    ///
+    /// Definition site. Undef previous forward declaration and provide
+    ///  actual definition.
+    ///
     ///  #undef MY_MACRO
     ///  #define MY_MACRO(arg0, arg1) (arg0 + arg1)
     #define SYNTROPY_MACRO_DECLARATION(...) \
