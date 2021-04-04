37abe40d4c73bf13d2d8b12f5a0f3467fcc3896d
Raffaele Daniele Facendola <raffaele.facendola@live.com>
2021-04-04 13:44:09 +0200

    Cleaning up.

--- extra/simple_compliance/src/simple_compliance/simple_compliance.cpp
+++ extra/simple_compliance/src/simple_compliance/simple_compliance.cpp
@@ -9,42 +9,9 @@
 
 #include "simple_compliance/bulk.h"
 
-struct Foo
-{
-
-};
-
-struct Bar : public Foo
-{
-    Bar() = default;
-};
-
-void DoBar(Syntropy::Tuple<Bar&> t)
-{
-
-}
-
 int main()
 {
     std::cout << "Hello, Syntropy\n";
-
-    Bar b;
-
-    Syntropy::Tuple<Foo&> p{b};
-    Syntropy::Tuple<Bar&> q{b};
-
-    Syntropy::Tuple<Foo&> r(b);
-    Syntropy::Tuple<Bar&> s(b);
-
-    auto t = Syntropy::Tuple<Foo&>{b};
-    auto u = Syntropy::Tuple<Bar&>{b};
-
-    auto v = Syntropy::Tuple<Foo&>(b);
-    auto z = Syntropy::Tuple<Bar&>(b);
-
-    std::cout << "Nice\n";
-
-    DoBar(b);
 
     std::cout << "\ngg\n";
 
