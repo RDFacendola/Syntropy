f3c7b1b06f554aa94933c25a9554d96eb03e24dc
Raffaele Daniele Facendola <raffaele.facendola@live.com>
2021-04-08 19:15:04 +0200

    Updating bulk file.

--- extra/simple_compliance/include/simple_compliance/bulk.h
+++ extra/simple_compliance/include/simple_compliance/bulk.h
@@ -6,8 +6,15 @@
 
 // =========================================================================  //
 
+// Core.
+// =====
+
+#include "syntropy/core/algorithms/swap.h"
+#include "syntropy/core/algorithms/compare.h"
+
 #include "syntropy/core/containers/fix_array.h"
 
+#include "syntropy/core/foundation/ordering.h"
 #include "syntropy/core/foundation/unique_ptr.h"
 
 #include "syntropy/core/ranges/bidirectional_range.h"
@@ -22,32 +29,43 @@
 #include "syntropy/core/ranges/span.h"
 #include "syntropy/core/ranges/zip_range.h"
 
+#include "syntropy/core/records/record.h"
+#include "syntropy/core/records/tuple.h"
+
 #include "syntropy/core/reflection/type_id.h"
 
-#include "syntropy/core/strings/string.h"
 #include "syntropy/core/strings/string_view.h"
+#include "syntropy/core/strings/string.h"
 
 #include "syntropy/core/support/event.h"
 #include "syntropy/core/support/scope_guard.h"
 #include "syntropy/core/support/visitor.h"
-#include "syntropy/core/algorithms/swap.h"
-#include "syntropy/core/algorithms/compare.h"
 
-#include "syntropy/core/records/record.h"
-#include "syntropy/core/records/tuple.h"
+// Diagnostics.
+// ============
 
 #include "syntropy/diagnostics/foundation/assert.h"
 #include "syntropy/diagnostics/foundation/debugger.h"
+#include "syntropy/diagnostics/foundation/source_location.h"
+
+// HAL.
+// ====
 
 #include "syntropy/hal/hal_macro.h"
 
+// Language.
+// =========
+
 #include "syntropy/language/foundation/conversions.h"
 #include "syntropy/language/foundation/definitions.h"
+#include "syntropy/language/foundation/foundation.h"
 #include "syntropy/language/foundation/types.h"
+
 #include "syntropy/language/preprocessor/macro.h"
 
 #include "syntropy/language/support/ignore.h"
 #include "syntropy/language/support/initializer_list.h"
+
 #include "syntropy/language/templates/concepts.h"
 #include "syntropy/language/templates/diagnostics.h"
 #include "syntropy/language/templates/invoke.h"
@@ -58,7 +76,16 @@
 #include "syntropy/language/templates/templates.h"
 #include "syntropy/language/templates/type_traits.h"
 
+// Math.
+// =====
+
 #include "syntropy/math/math.h"
+
+// Memory.
+// =======
+
+#include "syntropy/memory/allocators/allocator.h"
+#include "syntropy/memory/allocators/system_allocator.h"
 
 #include "syntropy/memory/foundation/address.h"
 #include "syntropy/memory/foundation/alignment.h"
@@ -68,7 +95,5 @@
 #include "syntropy/memory/foundation/memory.h"
 #include "syntropy/memory/foundation/size.h"
 
-#include "syntropy/memory/allocators/allocator.h"
-#include "syntropy/memory/allocators/system_allocator.h"
 
 // =========================================================================  //
