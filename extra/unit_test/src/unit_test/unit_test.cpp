d5d357ada1356489b9050f683c08c17b138a4e60
Raffaele Daniele Facendola <raffaele.facendola@live.com>
2021-04-11 22:47:14 +0200

    Adding UnitTest project.

---
+++ extra/unit_test/src/unit_test/unit_test.cpp
@@ -0,0 +1,18 @@
+/// \file unit_test.cpp
+///
+/// Runs the entire Syntropy test suite.
+///
+/// \author Raffaele D. Facendola - April 2021
+
+// ========================================================================= //
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello, Syntropy Unit Test\n";
+
+    return 0;
+}
+
+// ========================================================================= //
