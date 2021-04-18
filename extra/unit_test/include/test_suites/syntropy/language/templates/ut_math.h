65cc2fda7674492e42a3eac8798edfde584a1b0a
Raffaele Daniele Facendola <raffaele.facendola@live.com>
2021-04-18 14:24:40 +0200

    Adding aditional unit test for ratios and removing old math unit test header.

--- extra/unit_test/include/test_suites/syntropy/language/templates/ut_math.h
+++ extra/unit_test/include/test_suites/syntropy/language/templates/ut_math.h
@@ -45,6 +45,26 @@ namespace UnitTest
         SYNTROPY_UNIT_EQUAL((GCD<1, 42>), 1);
     })
 
+    .TestCase(u8"The greatest common divisor of a number and 0 is the number "
+              u8"itself.",
+    [](auto& fixture)
+    {
+        using namespace Syntropy;
+        using namespace Syntropy::Templates;
+
+        SYNTROPY_UNIT_EQUAL((GCD<42, 0>), 42);
+    })
+
+    .TestCase(u8"The greatest common divisor of a number and itself is the "
+              u8"number itself.",
+    [](auto& fixture)
+    {
+        using namespace Syntropy;
+        using namespace Syntropy::Templates;
+
+        SYNTROPY_UNIT_EQUAL((GCD<42, 42>), 42);
+    })
+
     .TestCase(u8"The greatest common divisor function is commutative.",
     [](auto& fixture)
     {
@@ -53,6 +73,24 @@ namespace UnitTest
 
         SYNTROPY_UNIT_EQUAL((GCD<42, 28>), 14);
         SYNTROPY_UNIT_EQUAL((GCD<28, 42>), 14);
+    })
+
+    .TestCase(u8"The greatest common divisor of two prime numbers is 1.",
+    [](auto& fixture)
+    {
+        using namespace Syntropy;
+        using namespace Syntropy::Templates;
+
+        SYNTROPY_UNIT_EQUAL((GCD<17, 23>), 1);
+    })
+
+    .TestCase(u8"The greatest common divisor of two coprime numbers is 1.",
+    [](auto& fixture)
+    {
+        using namespace Syntropy;
+        using namespace Syntropy::Templates;
+
+        SYNTROPY_UNIT_EQUAL((GCD<49, 81>), 1);
     })
 
     .TestCase(u8"The least common multiple of a number and 1 is the number "
@@ -65,6 +103,16 @@ namespace UnitTest
         SYNTROPY_UNIT_EQUAL((LCM<42, 1>), 42);
     })
 
+    .TestCase(u8"The least common multiple of a number and itself is the "
+              u8"number itself.",
+    [](auto& fixture)
+    {
+        using namespace Syntropy;
+        using namespace Syntropy::Templates;
+
+        SYNTROPY_UNIT_EQUAL((LCM<42, 42>), 42);
+    })
+
     .TestCase(u8"The least common multiple function is commutative.",
     [](auto& fixture)
     {
@@ -73,6 +121,16 @@ namespace UnitTest
 
         SYNTROPY_UNIT_EQUAL((LCM<42, 35>), 210);
         SYNTROPY_UNIT_EQUAL((LCM<35, 42>), 210);
+    })
+
+    .TestCase(u8"The least common multiple of two prime numbers is their "
+              u8"product.",
+    [](auto& fixture)
+    {
+        using namespace Syntropy;
+        using namespace Syntropy::Templates;
+
+        SYNTROPY_UNIT_EQUAL((LCM<17, 23>), 391);
     });
 
 }
