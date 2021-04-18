9a366f681b28cdd19faf8e9634de75dbe62c1631
Raffaele Daniele Facendola <raffaele.facendola@live.com>
2021-04-18 15:10:30 +0200

    Adding additional ratio unit test.

--- extra/unit_test/include/test_suites/syntropy/language/templates/ut_ratio.h
+++ extra/unit_test/include/test_suites/syntropy/language/templates/ut_ratio.h
@@ -35,6 +35,35 @@ namespace UnitTest
         = Syntropy::UnitTest::MakeAutoUnitTest<RatioTestFixture>(
             u8"ratio.templates.language.syntropy")
 
+    .TestCase(u8"A ratio-type is a ratio.",
+    [](auto& fixture)
+    {
+        using namespace Syntropy;
+        using namespace Syntropy::Templates;
+
+        SYNTROPY_UNIT_EQUAL((IsRatio<Ratio<1, 2>>), true);
+    })
+
+    .TestCase(u8"A non-ratio-type is not a ratio.",
+    [](auto& fixture)
+    {
+        using namespace Syntropy;
+        using namespace Syntropy::Templates;
+
+        SYNTROPY_UNIT_EQUAL((IsRatio<Int>), false);
+    })
+
+    .TestCase(u8"The common ratio of two ratios is a third ratio to which "
+              u8"both can be converted to.",
+    [](auto& fixture)
+    {
+        using namespace Syntropy;
+        using namespace Syntropy::Templates;
+
+        SYNTROPY_UNIT_SAME((CommonRatio<Ratio<3, 14>, Ratio<6, 21>>),
+                           (Ratio<3, 42>));
+    })
+
     .TestCase(u8"Adding two ratios returns the reduced sum of the twos.",
     [](auto& fixture)
     {
@@ -153,7 +182,155 @@ namespace UnitTest
 
         SYNTROPY_UNIT_SAME((RatioDivide<Ratio<1, 1>, Ratio<3, 6>>),
                            (Ratio<2, 1>));
+    })
+
+    .TestCase(u8"Ratios compare equal to themselves.",
+    [](auto& fixture)
+    {
+        using namespace Syntropy;
+        using namespace Syntropy::Templates;
+
+        SYNTROPY_UNIT_EQUAL((RatioEqual<Ratio<3, 6>, Ratio<3, 6>>), true);
+    })
+
+    .TestCase(u8"Ratios compare equal to their reduced form.",
+    [](auto& fixture)
+    {
+        using namespace Syntropy;
+        using namespace Syntropy::Templates;
+
+        SYNTROPY_UNIT_EQUAL((RatioEqual<Ratio<3, 6>, Ratio<1, 2>>), true);
+    })
+
+    .TestCase(u8"Ratios compare no different than themselves.",
+    [](auto& fixture)
+    {
+        using namespace Syntropy;
+        using namespace Syntropy::Templates;
+
+        SYNTROPY_UNIT_EQUAL((RatioNotEqual<Ratio<3, 6>, Ratio<3, 6>>), false);
+    })
+
+    .TestCase(u8"Ratios compare different to different ratios.",
+    [](auto& fixture)
+    {
+        using namespace Syntropy;
+        using namespace Syntropy::Templates;
+
+        SYNTROPY_UNIT_EQUAL((RatioNotEqual<Ratio<3, 6>, Ratio<4, 5>>), true);
+    })
+
+    .TestCase(u8"Ratios compare less-than larger ratios.",
+    [](auto& fixture)
+    {
+        using namespace Syntropy;
+        using namespace Syntropy::Templates;
+
+        SYNTROPY_UNIT_EQUAL((RatioLess<Ratio<3, 6>, Ratio<4, 5>>), true);
+    })
+
+    .TestCase(u8"Ratios compare not less-than smaller ratios.",
+    [](auto& fixture)
+    {
+        using namespace Syntropy;
+        using namespace Syntropy::Templates;
+
+        SYNTROPY_UNIT_EQUAL((RatioLess<Ratio<4, 5>, Ratio<3, 6>>), false);
+    })
+
+    .TestCase(u8"Ratios compare not less-than themselves.",
+    [](auto& fixture)
+    {
+        using namespace Syntropy;
+        using namespace Syntropy::Templates;
+
+        SYNTROPY_UNIT_EQUAL((RatioLess<Ratio<3, 6>, Ratio<1, 2>>), false);
+    })
+
+    .TestCase(u8"Ratios compare greater-than smaller ratios.",
+    [](auto& fixture)
+    {
+        using namespace Syntropy;
+        using namespace Syntropy::Templates;
+
+        SYNTROPY_UNIT_EQUAL((RatioGreater<Ratio<4, 5>, Ratio<3, 6>>), true);
+    })
+
+    .TestCase(u8"Ratios compare not greater-than larger ratios.",
+    [](auto& fixture)
+    {
+        using namespace Syntropy;
+        using namespace Syntropy::Templates;
+
+        SYNTROPY_UNIT_EQUAL((RatioGreater<Ratio<3, 6>, Ratio<4, 5>>), false);
+    })
+
+    .TestCase(u8"Ratios compare not greater-than themselves.",
+    [](auto& fixture)
+    {
+        using namespace Syntropy;
+        using namespace Syntropy::Templates;
+
+        SYNTROPY_UNIT_EQUAL((RatioGreater<Ratio<3, 6>, Ratio<1, 2>>), false);
+    })
+
+    .TestCase(u8"Ratios compare less-equal-than larger ratios.",
+    [](auto& fixture)
+    {
+        using namespace Syntropy;
+        using namespace Syntropy::Templates;
+
+        SYNTROPY_UNIT_EQUAL((RatioLessEqual<Ratio<3, 6>, Ratio<4, 5>>), true);
+    })
+
+    .TestCase(u8"Ratios compare not less-equal-than smaller ratios.",
+    [](auto& fixture)
+    {
+        using namespace Syntropy;
+        using namespace Syntropy::Templates;
+
+        SYNTROPY_UNIT_EQUAL((RatioLessEqual<Ratio<4, 5>, Ratio<3, 6>>), false);
+    })
+
+    .TestCase(u8"Ratios compare less-equal-than themselves.",
+    [](auto& fixture)
+    {
+        using namespace Syntropy;
+        using namespace Syntropy::Templates;
+
+        SYNTROPY_UNIT_EQUAL((RatioLessEqual<Ratio<3, 6>, Ratio<1, 2>>), true);
+    })
+
+    .TestCase(u8"Ratios compare greater-equal-than larger ratios.",
+    [](auto& fixture)
+    {
+        using namespace Syntropy;
+        using namespace Syntropy::Templates;
+
+        SYNTROPY_UNIT_EQUAL((RatioGreaterEqual<Ratio<4, 5>, Ratio<3, 6>>),
+                            true);
+    })
+
+    .TestCase(u8"Ratios compare not greater-equal-than smaller ratios.",
+    [](auto& fixture)
+    {
+        using namespace Syntropy;
+        using namespace Syntropy::Templates;
+
+        SYNTROPY_UNIT_EQUAL((RatioGreaterEqual<Ratio<3, 6>, Ratio<4, 5>>),
+                            false);
+    })
+
+    .TestCase(u8"Ratios compare greater-equal-than themselves.",
+    [](auto& fixture)
+    {
+        using namespace Syntropy;
+        using namespace Syntropy::Templates;
+
+        SYNTROPY_UNIT_EQUAL((RatioGreaterEqual<Ratio<3, 6>, Ratio<1, 2>>),
+                            true);
     });
+
 }
 
 // ===========================================================================
