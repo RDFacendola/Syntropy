a9d049c54a43733e544de036b0be8a0d39ed498f
Raffaele Daniele Facendola <raffaele.facendola@live.com>
2021-05-02 18:45:21 +0200

    Moving Invoke traits under type_traits.h

--- syntropy/include/syntropy/language/templates/invoke.h
+++ syntropy/include/syntropy/language/templates/invoke.h
@@ -24,31 +24,6 @@ namespace Syntropy::Templates
     /* INVOKE                                                               */
     /************************************************************************/
 
-    /// \brief List of types needed to perform a callable object invocation.
-    ///
-    /// \remarks For member-functions, the first argument in the list is the
-    ///          (possibly) qualified type of the class the function belongs
-    ///          to. For functor objects the calling instance is implicit,
-    ///          therefore its type is not present in the argument list.
-    template <typename TCallable>
-    using InvokeArgumentsOf
-        = Details::InvokeArgumentsOf<TCallable>;
-
-    /// \brief Provides indexed access to an invocation argument type.
-    template <Int TIndex, typename TCallable >
-    using InvokeArgumentOf
-        = ElementTypeOf<TIndex, InvokeArgumentsOf<TCallable>>;
-
-    /// \brief Result type of a callable object invocation.
-    ///
-    /// \remarks In case of member and non-member functions the argument list
-    ///          is not necessary to determine the correct return type.
-    ///          For functors, however, there might be several overloads,
-    ///          therefore the argument types must be specified.
-    template <typename TCallable, typename... TArguments>
-    using InvokeResultOf
-        = Details::InvokeResultOf<TCallable, TArguments...>;
-
     /// \brief Invoke a callable object with provided arguments.
     ///
     /// \usage Invoke(functor, arguments...); // Calls functor(arguments...)
